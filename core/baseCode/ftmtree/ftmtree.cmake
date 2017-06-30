ca7fd315406abd5ac0936c747f64563aa341ec86
Julien J Tierny <julien.tierny@gmail.com>
2017-06-30 14:40:01 +0200

    Added CMake ttk_add_external_header_package()
    For eigen, boost, etc.

--- core/baseCode/ftmtree/ftmtree.cmake
+++ core/baseCode/ftmtree/ftmtree.cmake
@@ -2,8 +2,6 @@
 ttk_add_basecode_package(triangulation)
 
 # Boost dependencies
-ttk_add_external_package(boost boost)
-
-set(CMAKE_CXX_FLAGS "-D_GLIBCXX_PARALLEL")
+ttk_add_external_header_package(boost)
 
 ttk_wrapup_library(libFTMTree "FTMTree.cpp FTMTree_CT.cpp FTMTree_MT.cpp Segmentation.cpp")
