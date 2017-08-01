452c1cf74cd0471888c4de6c42eb995e4984a4e2
Charles Gueunet <charles.gueunet@lip6.fr>
2017-08-01 11:14:13 +0200

    style(function order): Segmentation.cpp

--- core/baseCode/ftmtree/Segmentation.cpp
+++ core/baseCode/ftmtree/Segmentation.cpp
@@ -21,16 +21,22 @@
 using namespace ttk;
 using namespace std;
 
+// -------
+// Segment
+// -------
+
 Segment::Segment(idVertex size) : vertices_(size, nullVertex)
 {
 }
 
-void Segment::sort(const Scalars* s)
+segm_const_it Segment::begin(void) const
 {
-   // Sort by scalar value
-   auto comp = [&](idVertex a, idVertex b) { return s->isLower(a, b); };
+   return vertices_.begin();
+}
 
-   std::sort(vertices_.begin(), vertices_.end(), comp);
+segm_it Segment::begin(void)
+{
+   return vertices_.begin();
 }
 
 void Segment::createFromList(const Scalars* s, list<vector<idVertex>>& regularList, const bool reverse)
@@ -64,29 +70,14 @@ void Segment::createFromList(const Scalars* s, list<vector<idVertex>>& regularLi
    regularList.clear();
 }
 
-segm_const_it Segment::begin(void) const
-{
-   return vertices_.begin();
-}
-
 segm_const_it Segment::end(void) const
 {
    return vertices_.end();
 }
 
-segm_it Segment::begin(void)
-{
-   return vertices_.begin();
-}
-
 segm_it Segment::end(void)
 {
    return vertices_.end();
-}
-
-idVertex Segment::size(void) const
-{
-   return vertices_.size();
 }
 
 idVertex Segment::operator[](const size_t& idx) const
@@ -99,19 +90,40 @@ idVertex& Segment::operator[](const size_t& idx)
    return vertices_[idx];
 }
 
+idVertex Segment::size(void) const
+{
+   return vertices_.size();
+}
+
+void Segment::sort(const Scalars* s)
+{
+   // Sort by scalar value
+   auto comp = [&](idVertex a, idVertex b) { return s->isLower(a, b); };
+
+   std::sort(vertices_.begin(), vertices_.end(), comp);
+}
+
+// --------
+// Segments
+// --------
+
 Segments::Segments()
 {
 }
 
-void Segments::sortAll(const Scalars* s)
+void Segments::clear(void)
 {
-   const idSegment& nbSegments = size();
+    segments_.clear();
+}
 
-   for (idSegment i = 0; i < nbSegments; i++) {
-#pragma omp task firstprivate(i)
-      segments_[i].sort(s);
-   }
-#pragma omp taskwait
+Segment& Segments::operator[](const size_t& idx)
+{
+   return segments_[idx];
+}
+
+const Segment& Segments::operator[](const size_t& idx) const
+{
+   return segments_[idx];
 }
 
 void Segments::resize(const vector<idVertex>& sizes)
@@ -133,20 +145,20 @@ idSegment Segments::size(void) const
    return segments_.size();
 }
 
-void Segments::clear(void)
+void Segments::sortAll(const Scalars* s)
 {
-    segments_.clear();
+   const idSegment& nbSegments = size();
+
+   for (idSegment i = 0; i < nbSegments; i++) {
+#pragma omp task firstprivate(i)
+      segments_[i].sort(s);
+   }
+#pragma omp taskwait
 }
 
-Segment& Segments::operator[](const size_t& idx)
-{
-   return segments_[idx];
-}
-
-const Segment& Segments::operator[](const size_t& idx) const
-{
-   return segments_[idx];
-}
+// ----------
+// Arc Region
+// ----------
 
 ArcRegion::ArcRegion()
 {
@@ -158,6 +170,145 @@ ArcRegion::ArcRegion()
 ArcRegion::ArcRegion(const segm_it& begin, const segm_it& end) : ArcRegion()
 {
    concat(begin, end);
+}
+
+void ArcRegion::concat(const segm_it& begin, const segm_it& end)
+{
+   segmentsIn_.emplace_front(Region{begin, end});
+}
+
+void ArcRegion::concat(const ArcRegion& r)
+{
+   for (const auto& reg : r.segmentsIn_) {
+      concat(reg.segmentBegin, reg.segmentEnd);
+   }
+}
+
+void ArcRegion::createSegmentation(const Scalars* s)
+{
+#ifndef withKamikaze
+   if (segmentation_.size()) {
+      cout << "createSegmentation called on an already segmented region" << endl;
+   }
+#endif
+
+   idVertex              totalSegmSize = 0;
+   vector<segm_const_it> heads, ends;
+   for (const auto& region : segmentsIn_) {
+      totalSegmSize += distance(region.segmentBegin, region.segmentEnd);
+      heads.emplace_back(region.segmentBegin);
+      ends.emplace_back(region.segmentEnd);
+   }
+
+   segmentation_.clear();
+   segmentation_.reserve(totalSegmSize);  // max size, including discarded vertices
+
+   idSegment nbSegments = heads.size();
+   int       added      = 0;
+
+   while (added != -1) {
+      added = -1;
+      idVertex minVert;
+      for (idSegment i = 0; i < nbSegments; i++) {
+         auto&       headIt = heads[i];
+         const auto& endIt  = ends[i];
+
+         if (headIt == endIt) {
+            // end of this area
+            heads.erase(heads.begin() + i);
+            ends.erase(ends.begin() + i);
+            --nbSegments;
+            --i;
+            continue;
+         }
+
+         if (added == -1 || s->isLower(*headIt, minVert)) {
+            minVert = *headIt;
+            added   = i;
+         }
+      }
+      if (added != -1) {
+         segmentation_.emplace_back(minVert);
+         ++heads[added];
+      }
+   }  // end while
+
+#ifndef withKamikaze
+   segmented_ = true;
+#endif
+}
+
+idVertex ArcRegion::findBelow(idVertex v, const Scalars* s,
+                              const vector<idCorresp>& vert2treeOther) const
+{
+   // split at v and return remaining vertices
+
+   auto comp            = [s](idVertex a, idVertex b) { return s->isLower(a, b); };
+   idVertex   splitVert = nullVertex;
+   const bool chkOther  = vert2treeOther.size() > 0;
+
+   for (const auto& reg : segmentsIn_) {
+      if (s->isEqLower(*reg.segmentBegin, v) && s->isEqHigher(*(reg.segmentEnd - 1), v)) {
+         // is v is between beg/end
+         // append once
+         const auto& oldBeg = reg.segmentBegin;
+         auto        posV   = lower_bound(oldBeg, reg.segmentEnd, v, comp);
+
+         // posV == end() would mean v is not in this range (cf if in for)
+         if (posV != oldBeg && *posV != v) {
+            --posV;
+         }
+
+         if (chkOther) {
+            while (posV != oldBeg && vert2treeOther[*posV] == nullCorresp) {
+               --posV;
+            }
+            if (posV == oldBeg && vert2treeOther[*posV] == nullCorresp) {
+               continue;
+            }
+         }
+
+         if (posV != reg.segmentEnd) {
+            splitVert = *posV;
+         }
+
+      } else if (s->isLower(*(reg.segmentEnd - 1), v)) {
+         // reg is completely below v, we give it to remainingRegion
+         if (splitVert == nullVertex || s->isHigher(*(reg.segmentEnd - 1), splitVert)) {
+            auto posV = reg.segmentEnd - 1;
+            if (chkOther) {
+               const auto& oldBeg = reg.segmentBegin;
+               while (posV != oldBeg && vert2treeOther[*posV] == nullCorresp) {
+                  --posV;
+               }
+               if (((posV == oldBeg && vert2treeOther[*posV] != nullCorresp) || posV != oldBeg) &&
+                   (splitVert == nullVertex || s->isHigher(*posV, splitVert))) {
+                  splitVert = *posV;
+               }
+            } else {
+               splitVert = *posV;
+            }
+         }
+      }
+   }
+
+   return splitVert;
+}
+
+bool ArcRegion::merge(const ArcRegion& r)
+{
+   const Region& other = r.segmentsIn_.front();
+   Region&       self  = segmentsIn_.front();
+
+   if (other.segmentBegin == self.segmentEnd) {
+      self.segmentEnd = other.segmentEnd;
+      return true;
+   } else if (other.segmentEnd == self.segmentBegin) {
+      self.segmentBegin = other.segmentBegin;
+      return true;
+   }
+
+   return false;
 }
 
 tuple<idVertex, ArcRegion> ArcRegion::splitFront(idVertex v, const Scalars* s)
@@ -258,143 +409,4 @@ tuple<idVertex, ArcRegion> ArcRegion::splitBack(idVertex v, const Scalars* s)
    }
 
    return make_tuple(splitVert, remainingRegion);
-}
-
-idVertex ArcRegion::findBelow(idVertex v, const Scalars* s,
-                              const vector<idCorresp>& vert2treeOther) const
-{
-   // split at v and return remaining vertices
-
-   auto comp            = [s](idVertex a, idVertex b) { return s->isLower(a, b); };
-   idVertex   splitVert = nullVertex;
-   const bool chkOther  = vert2treeOther.size() > 0;
-
-   for (const auto& reg : segmentsIn_) {
-      if (s->isEqLower(*reg.segmentBegin, v) && s->isEqHigher(*(reg.segmentEnd - 1), v)) {
-         // is v is between beg/end
-         // append once
-         const auto& oldBeg = reg.segmentBegin;
-         auto        posV   = lower_bound(oldBeg, reg.segmentEnd, v, comp);
-
-         // posV == end() would mean v is not in this range (cf if in for)
-         if (posV != oldBeg && *posV != v) {
-            --posV;
-         }
-
-         if (chkOther) {
-            while (posV != oldBeg && vert2treeOther[*posV] == nullCorresp) {
-               --posV;
-            }
-            if (posV == oldBeg && vert2treeOther[*posV] == nullCorresp) {
-               continue;
-            }
-         }
-
-         if (posV != reg.segmentEnd) {
-            splitVert = *posV;
-         }
-
-      } else if (s->isLower(*(reg.segmentEnd - 1), v)) {
-         // reg is completely below v, we give it to remainingRegion
-         if (splitVert == nullVertex || s->isHigher(*(reg.segmentEnd - 1), splitVert)) {
-            auto posV = reg.segmentEnd - 1;
-            if (chkOther) {
-               const auto& oldBeg = reg.segmentBegin;
-               while (posV != oldBeg && vert2treeOther[*posV] == nullCorresp) {
-                  --posV;
-               }
-               if (((posV == oldBeg && vert2treeOther[*posV] != nullCorresp) || posV != oldBeg) &&
-                   (splitVert == nullVertex || s->isHigher(*posV, splitVert))) {
-                  splitVert = *posV;
-               }
-            } else {
-               splitVert = *posV;
-            }
-         }
-      }
-   }
-
-   return splitVert;
-}
-
-void ArcRegion::concat(const segm_it& begin, const segm_it& end)
-{
-   segmentsIn_.emplace_front(Region{begin, end});
-}
-
-void ArcRegion::concat(const ArcRegion& r)
-{
-   for (const auto& reg : r.segmentsIn_) {
-      concat(reg.segmentBegin, reg.segmentEnd);
-   }
-}
-
-bool ArcRegion::merge(const ArcRegion& r)
-{
-   const Region& other = r.segmentsIn_.front();
-   Region&       self  = segmentsIn_.front();
-
-   if (other.segmentBegin == self.segmentEnd) {
-      self.segmentEnd = other.segmentEnd;
-      return true;
-   } else if (other.segmentEnd == self.segmentBegin) {
-      self.segmentBegin = other.segmentBegin;
-      return true;
-   }
-
-   return false;
-}
-
-void ArcRegion::createSegmentation(const Scalars* s)
-{
-#ifndef withKamikaze
-   if (segmentation_.size()) {
-      cout << "createSegmentation called on an already segmented region" << endl;
-   }
-#endif
-
-   idVertex              totalSegmSize = 0;
-   vector<segm_const_it> heads, ends;
-   for (const auto& region : segmentsIn_) {
-      totalSegmSize += distance(region.segmentBegin, region.segmentEnd);
-      heads.emplace_back(region.segmentBegin);
-      ends.emplace_back(region.segmentEnd);
-   }
-
-   segmentation_.clear();
-   segmentation_.reserve(totalSegmSize);  // max size, including discarded vertices
-
-   idSegment nbSegments = heads.size();
-   int       added      = 0;
-
-   while (added != -1) {
-      added = -1;
-      idVertex minVert;
-      for (idSegment i = 0; i < nbSegments; i++) {
-         auto&       headIt = heads[i];
-         const auto& endIt  = ends[i];
-
-         if (headIt == endIt) {
-            // end of this area
-            heads.erase(heads.begin() + i);
-            ends.erase(ends.begin() + i);
-            --nbSegments;
-            --i;
-            continue;
-         }
-
-         if (added == -1 || s->isLower(*headIt, minVert)) {
-            minVert = *headIt;
-            added   = i;
-         }
-      }
-      if (added != -1) {
-         segmentation_.emplace_back(minVert);
-         ++heads[added];
-      }
-   }  // end while
-
-#ifndef withKamikaze
-   segmented_ = true;
-#endif
 }
