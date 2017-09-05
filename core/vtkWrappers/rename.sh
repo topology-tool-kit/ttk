4a30ed2dde6525cf42f84680ab496232ba8ed9c6
Will Usher <will@sci.utah.edu>
2017-09-05 11:40:14 -0600

    remove accidentally added script

--- core/vtkWrappers/rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for f in `ls -d vtk*`; do
-	ttk_dir=`echo $f | sed s/vtk/ttk/`
-	sed -ri "s/vtk([A-Z])/ttk\1/g" $f/CMakeLists.txt
-	mv $f/CMakeLists.txt $ttk_dir/CMakeLists.txt
-done
-
