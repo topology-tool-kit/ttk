49c62575a02106b1b3fd6dcd25cd4f0bb0f30627
Will Usher <will@sci.utah.edu>
2017-09-05 15:56:29 -0600

    Cleaning up older stuff, trying to get a paraview plugin working
    Seems like they do some bs trying to wrap your filters or some kind of
    crap so if your filters are actually in a compiled lib they don't pick
    them up???

--- core/baseCode/oneSkeleton/oneSkeleton.cmake
+++
@@ -1,4 +0,0 @@
-ttk_add_baseCode_package(zeroSkeleton)
-
-# if the package is not a template, uncomment the following line
-ttk_wrapup_library(libOneSkeleton "OneSkeleton.cpp")
---
+++ paraview/ContourForests/ttkPVContourForests.cpp
@@ -0,0 +1,4 @@
+// This file is empty, but if you needed additional plugin code
+// you could extend the TTK base code or VTK wrappers further from here.
+#include "ttkContourForests.h"
+
