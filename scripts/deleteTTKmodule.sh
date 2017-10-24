7371693e7fd173df06ab78b1cbe9cc5e8e44fb34
Julien Tierny <julien.tierny@gmail.com>
2017-10-24 06:53:26 +0200

    Updated scripts for removed prefix.

--- scripts/deleteTTKmodule.sh
+++ scripts/deleteTTKmodule.sh
@@ -27,6 +27,6 @@ echo "Deleting TTK module ${Name}..."
 rm -R core/base/${smallName}  2> /dev/null
 rm -R core/vtk/ttk${Name} 2> /dev/null
 rm -R standalone/${Name} 2> /dev/null
-rm -R paraview/ttk${Name} 2> /dev/null
+rm -R paraview/${Name} 2> /dev/null
 
 echo "Module deleted."
