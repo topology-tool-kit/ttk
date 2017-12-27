3963cb0c7e4584b07a0aeffe6350cd00d8ac0421
Julien J Tierny <julien.tierny@gmail.com>
2017-12-27 16:17:18 +0100

    Mutualize unix and windows code (ttkUncertainDEst)

--- core/vtk/ttkUncertainDataEstimator/ttkUncertainDataEstimator.h
+++ core/vtk/ttkUncertainDataEstimator/ttkUncertainDataEstimator.h
@@ -27,27 +27,6 @@
 #ifndef _TTK_UNCERTAINDATAESTIMATOR_H
 #define _TTK_UNCERTAINDATAESTIMATOR_H
 
-#ifndef _MSC_VER
-// ttk code includes
-#include                  <UncertainDataEstimator.h>
-#include                  <Wrapper.h>
-
-// VTK includes -- to adapt
-#include                  <vtkCharArray.h>
-#include                  <vtkDataArray.h>
-#include                  <vtkDataSet.h>
-#include                  <vtkDataSetAlgorithm.h>
-#include                  <vtkDoubleArray.h>
-#include                  <vtkFiltersCoreModule.h>
-#include                  <vtkFloatArray.h>
-#include                  <vtkInformation.h>
-#include                  <vtkInformationVector.h>
-#include                  <vtkIntArray.h>
-#include                  <vtkObjectFactory.h>
-#include                  <vtkPointData.h>
-#include                  <vtkSmartPointer.h>
-#include                  <vtkTable.h>
-#else
 // VTK includes -- to adapt
 #include                  <vtkCharArray.h>
 #include                  <vtkDataArray.h>
@@ -67,7 +46,6 @@
 // ttk code includes
 #include                  <UncertainDataEstimator.h>
 #include                  <Wrapper.h>
-#endif
 
 // in this example, this wrapper takes a data-set on the input and produces a
 // data-set on the output - to adapt.
