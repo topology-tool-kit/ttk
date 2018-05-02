343adb8bd3bf0e3e8aa569b631354ba0aff6e64a
Charles Gueunet <charles.gueunet@gmail.com>
2018-05-02 10:30:07 +0200

    fix(Syntax): add missing parenthesis

--- core/base/ftmTree/FTMTree_Template.h
+++ core/base/ftmTree/FTMTree_Template.h
@@ -59,7 +59,7 @@ void ftm::FTMTree::build(void)
 #pragma omp parallel for
 #endif
       for (idVertex i = 0; i < scalars_->size; i++) {
-         if (::std::isnan((double)(((scalarType*)scalars_->values)[i])) {
+         if (::std::isnan((double)(((scalarType*)scalars_->values)[i]))) {
             ((scalarType*)scalars_->values)[i] = 0;
          }
       }
