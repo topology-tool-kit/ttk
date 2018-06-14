1733ae1dfd0b1eaa489972f339b026ede3b23715
Julien J Tierny <julien.tierny@gmail.com>
2018-06-14 08:22:43 +0200

    Added input detailed info for discrete gradient

--- core/base/discreteGradient/DiscreteGradient.h
+++ core/base/discreteGradient/DiscreteGradient.h
@@ -2002,8 +2002,20 @@ int ttk::DiscreteGradient::buildGradient(){
 
   {
     std::stringstream msg;
-    msg << "[DiscreteGradient] Data-set (" << numberOfVertices_
-      << " points) processed in "
+    msg << "[DiscreteGradient] Data-set: "
+      << numberOfVertices_ << " v., "
+      << inputTriangulation_->getNumberOfEdges() << " e.";
+    if(inputTriangulation_->getDimensionality() == 3){
+      msg << ", " << inputTriangulation_->getNumberOfTriangles()
+        << " t., " << inputTriangulation_->getNumberOfCells() 
+        << " T." << std::endl;
+    }
+    else if(inputTriangulation_->getDimensionality() == 2){
+      msg << ", " << inputTriangulation_->getNumberOfCells()
+        << " t." << std::endl;
+    }
+      
+    msg << "[DiscreteGradient] Processed in "
       << t.getElapsedTime() << " s. (" << threadNumber_
       << " thread(s))."
       << std::endl;
@@ -2025,8 +2037,8 @@ int ttk::DiscreteGradient::buildGradient2(){
 
   {
     std::stringstream msg;
-    msg << "[DiscreteGradient] Data-set (" << numberOfVertices_
-      << " points) post-processed in "
+      
+    msg << "[DiscreteGradient] Data-set post-processed in "
       << t.getElapsedTime() << " s. (" << threadNumber_
       << " thread(s))."
       << std::endl;
