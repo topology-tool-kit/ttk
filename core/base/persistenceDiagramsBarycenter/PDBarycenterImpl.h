ef4041c8619ca4cb972d5750c4ebd27054b1f2ce
Joseph BUDIN <joseph.budin@polytechnique.edu>
2018-08-01 09:59:56 +0200

    Slight modification

--- core/base/persistenceDiagramsBarycenter/PDBarycenterImpl.h
+++ core/base/persistenceDiagramsBarycenter/PDBarycenterImpl.h
@@ -138,8 +138,8 @@ std::vector<std::vector<matchingTuple>> PDBarycenter<dataType>::execute(std::vec
 				if(epsilon_candidate<epsilon){
 					epsilon=epsilon_candidate;
 				}
-				else{
-					epsilon *= 0.9;
+				else if (use_progressive_){
+					epsilon *= 0.95;
 				}
 			}
 			
