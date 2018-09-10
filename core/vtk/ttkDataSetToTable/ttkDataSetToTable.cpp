ba53caf8ec61bf8cc5ae3eb68103e8312d3aab0d
GuillaumeFavelier <guillaume.favelier@gmail.com>
2018-09-10 10:59:57 +0200

    Add a module to create a table from a vtkDataSet point or cell data

---
+++ core/vtk/ttkDataSetToTable/ttkDataSetToTable.cpp
@@ -0,0 +1,109 @@
+#include <ttkDataSetToTable.h>
+#include <regex>
+
+using namespace std;
+using namespace ttk;
+
+vtkStandardNewMacro(ttkDataSetToTable)
+
+  // transmit abort signals
+  bool ttkDataSetToTable::needsToAbort(){
+    return GetAbortExecute();
+  }
+
+// transmit progress status
+int ttkDataSetToTable::updateProgress(const float &progress){
+
+  {
+    stringstream msg;
+    msg << "[ttkDataSetToTable] " << progress*100 
+      << "% processed...." << endl;
+    dMsg(cout, msg.str(), advancedInfoMsg);
+  }
+
+  UpdateProgress(progress);
+  return 0;
+}
+
+int ttkDataSetToTable::doIt(vtkDataSet* input, vtkTable* output){
+  Memory m;
+
+  vtkSmartPointer<vtkTable> table=vtkSmartPointer<vtkTable>::New();
+
+  switch(DataAssociation){
+    case AssociationType::Point:
+      {
+        vtkPointData* inputPointData=input->GetPointData();
+#ifndef TTK_ENABLE_KAMIKAZE
+        if(!inputPointData){
+          cerr << "[ttkDataSetToTable] Error: input has no point data." << endl;
+          return -1;
+        }
+#endif
+
+        const int numberOfArrays=inputPointData->GetNumberOfArrays();
+#ifndef TTK_ENABLE_KAMIKAZE
+        if(numberOfArrays<=0){
+          cerr << "[ttkDataSetToTable] Error: input point data is empty." << endl;
+          return -1;
+        }
+#endif
+
+        table->GetRowData()->ShallowCopy(inputPointData);
+      }
+      break;
+
+    case AssociationType::Cell:
+      {
+        vtkCellData* inputCellData=input->GetCellData();
+#ifndef TTK_ENABLE_KAMIKAZE
+        if(!inputCellData){
+          cerr << "[ttkDataSetToTable] Error: input has no cell data." << endl;
+          return -1;
+        }
+#endif
+
+        const int numberOfArrays=inputCellData->GetNumberOfArrays();
+#ifndef TTK_ENABLE_KAMIKAZE
+        if(numberOfArrays<=0){
+          cerr << "[ttkDataSetToTable] Error: input cell data is empty." << endl;
+          return -1;
+        }
+#endif
+
+        table->GetRowData()->ShallowCopy(inputCellData);
+      }
+      break;
+  }
+
+  output->ShallowCopy(table);
+
+  {
+    stringstream msg;
+    msg << "[ttkDataSetToTable] Memory usage: " << m.getElapsedUsage()
+      << " MB." << endl;
+    dMsg(cout, msg.str(), memoryMsg);
+  }
+
+  return 0;
+}
+
+int ttkDataSetToTable::RequestData(vtkInformation *request,
+    vtkInformationVector **inputVector,
+    vtkInformationVector *outputVector){
+  Memory m;
+
+  vtkDataSet *input = vtkDataSet::GetData(inputVector[0]);
+  vtkTable *output = vtkTable::GetData(outputVector);
+
+  doIt(input, output);
+
+  {
+    stringstream msg;
+    msg << "[ttkDataSetToTable] Memory usage: " << m.getElapsedUsage()
+      << " MB." << endl;
+    dMsg(cout, msg.str(), memoryMsg);
+  }
+
+  return 1;
+}
---
+++ core/vtk/ttkDataSetToTable/ttkDataSetToTable.h
@@ -0,0 +1,130 @@
+/// \ingroup vtk
+/// \class ttkDataSetToTable
+/// \author Guillaume Favelier <guillaume.favelier@lip6.fr>
+/// \date December 2017
+///
+/// \brief TTK VTK-filter that selects scalar fields on input with shallow copy.
+/// 
+/// \param Input Input scalar field (vtkDataSet)
+/// \param Output Output scalar field (vtkDataSet)
+///
+/// This filter can be used as any other VTK filter (for instance, by using the 
+/// sequence of calls SetInputData(), Update(), GetOutput()).
+///
+/// See the related ParaView example state files for usage examples within a 
+/// VTK pipeline.
+#pragma once
+
+// VTK includes
+#include<vtkCharArray.h>
+#include<vtkDataArray.h>
+#include<vtkDataSet.h>
+#include<vtkDataSetAlgorithm.h>
+#include<vtkDoubleArray.h>
+#include<vtkFiltersCoreModule.h>
+#include<vtkFloatArray.h>
+#include<vtkInformation.h>
+#include<vtkIntArray.h>
+#include<vtkObjectFactory.h>
+#include<vtkPointData.h>
+#include<vtkCellData.h>
+#include<vtkSmartPointer.h>
+#include<vtkTable.h>
+
+// ttk code includes
+#include<Wrapper.h>
+
+#ifndef TTK_PLUGIN
+class VTKFILTERSCORE_EXPORT ttkDataSetToTable
+#else
+class ttkDataSetToTable
+#endif
+: public vtkDataSetAlgorithm, public ttk::Wrapper{
+
+  public:
+
+    enum AssociationType{
+      Point=0,
+      Cell
+    };
+
+    static ttkDataSetToTable* New();
+    vtkTypeMacro(ttkDataSetToTable, vtkDataSetAlgorithm)
+
+      // default ttk setters
+      vtkSetMacro(debugLevel_, int);
+
+    void SetThreads(){
+      if(!UseAllCores)
+        threadNumber_ = ThreadNumber;
+      else{
+        threadNumber_ = ttk::OsCall::getNumberOfCores();
+      }
+      Modified();
+    }
+
+    void SetThreadNumber(int threadNumber){\
+      ThreadNumber = threadNumber;\
+        SetThreads();\
+    }\
+    void SetUseAllCores(bool onOff){
+      UseAllCores = onOff;
+      SetThreads();
+    }
+    // end of default ttk setters
+
+    vtkSetMacro(DataAssociation, int);
+    vtkGetMacro(DataAssociation, int);
+
+    int FillInputPortInformation(int port, vtkInformation *info){
+      switch(port){
+        case 0:
+          info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkDataSet");
+          break;
+        default:
+          break;
+      }
+
+      return 1;
+    }
+
+    int FillOutputPortInformation(int port, vtkInformation *info){
+
+      switch(port){
+        case 0:
+          info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkTable");
+          break;
+        default:
+          break;
+      }
+
+      return 1;
+    }
+
+  protected:
+
+    ttkDataSetToTable(){
+      UseAllCores = true;
+      ThreadNumber = 1;
+      debugLevel_ = 3;
+
+      SetNumberOfInputPorts(1);
+      SetNumberOfOutputPorts(1);
+    }
+
+    ~ttkDataSetToTable(){};
+
+    int RequestData(vtkInformation *request,
+        vtkInformationVector **inputVector,
+        vtkInformationVector *outputVector);
+
+  private:
+
+    bool UseAllCores;
+    int ThreadNumber;
+    int DataAssociation;
+
+    int doIt(vtkDataSet* input, vtkTable* output);
+    bool needsToAbort();
+    int updateProgress(const float &progress);
+};
