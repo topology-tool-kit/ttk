3a9c6c9848c5abf1c9a2d6496ea26c796eb5bc00
Julien J Tierny <julien.tierny@gmail.com>
2018-10-06 16:10:16 +0200

    Version bump

--- core/base/common/Debug.h
+++ core/base/common/Debug.h
@@ -1,6 +1,6 @@
 /// \namespace ttk The Topology ToolKit
 
-/// \mainpage TTK 0.9.6 Documentation
+/// \mainpage TTK 0.9.7 Documentation
 /// \image html "../img/splash.png"
 /// Useful links:
 ///   - TTK Home: 
