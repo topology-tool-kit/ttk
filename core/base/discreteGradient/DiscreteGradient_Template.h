b89c39ef2d0e3c0cc3a9667806ed1f7776f1432b
GuillaumeFavelier <guillaume.favelier@gmail.com>
2018-11-30 13:08:27 +0100

    fix assignGradient

--- core/base/discreteGradient/DiscreteGradient_Template.h
+++ core/base/discreteGradient/DiscreteGradient_Template.h
@@ -642,9 +642,9 @@ int DiscreteGradient::assignGradient2(const int alphaDim,
             gradient[alphaDim][alpha] = minStarLocalId;
 
             char minAlphaLocalId{-1};
-            for(SimplexId k=0; k<3; ++k){
+            for(SimplexId k=0; k<4; ++k){
               SimplexId tmp;
-              inputTriangulation_->getTriangleEdge(minStarId, k, tmp);
+              inputTriangulation_->getCellTriangle(minStarId, k, tmp);
               if(tmp == alpha){
                 minAlphaLocalId = k;
                 break;
@@ -733,9 +733,9 @@ int DiscreteGradient::assignGradient3(const int alphaDim,
           gradient[alphaDim][alpha] = minStarLocalId;
 
           char minAlphaLocalId{-1};
-          for(SimplexId k=0; k<3; ++k){
+          for(SimplexId k=0; k<4; ++k){
             SimplexId tmp;
-            inputTriangulation_->getTriangleEdge(minStarId, k, tmp);
+            inputTriangulation_->getCellTriangle(minStarId, k, tmp);
             if(tmp == alpha){
               minAlphaLocalId = k;
               break;
