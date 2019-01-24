99bda1ac5f3173b6dbe762b4a9b7cc4c12e59ba1
Julien Tierny <julien.tierny@gmail.com>
2019-01-24 11:08:43 +0100

    Fixed FTM standalone.

--- standalone/FTMTree/cmd/main.cpp
+++ standalone/FTMTree/cmd/main.cpp
@@ -1,27 +1,43 @@
-/*
- * file:                  main.cpp
- * description:           dummy program example.
- * author:                Your Name Here <Your Email Address Here>.
- * date:                  The Date Here.
- */
+/// \author Julien Tierny <julien.tierny@sorbonne-universite.fr>.
+/// \date January 2019.
+///
+/// \brief FTM algorithm standalone binary.
 
 // include the local headers
-#include                  "Editor.h"
+#include                  <ttkFTMTree.h>
+#include                  <ttkProgramBase.h>
 
 using namespace std;
 using namespace ttk;
 
 int main(int argc, char **argv) {
 
-  // init editor
-  Editor editor;
-  editor.init(argc, argv);
+  vtkProgram<ttkFTMTree> program;
+  
+  int fieldId = 0;
+  int treeType = 0;
 
+  program.parser_.setArgument("f", &fieldId,
+    "Field identifier", true);
+  program.parser_.setArgument("T", &treeType,
+    "Tree type {0: JT, 1: ST, 2: CT}", true);
+  
+  int ret = 0;
+  ret = program.init(argc, argv);
+ 
+  if(ret != 0)
+    return ret;
+
+  program.ttkObject_->SetScalarFieldId(fieldId);
+  program.ttkObject_->SetTreeType(treeType);
+  
   // execute data processing
-  editor.execute();
-
-  // save the output
-  //editor.saveData("output.vti");
-
-  return 0;
+  ret = program.run();
+  
+  if(ret != 0)
+    return ret;
+ 
+  ret = program.save();
+  
+  return ret;
 }
