6a40777bd936a07926f730869a407bbf1eb26957
Pierre Guillou <pierre.guillou@lip6.fr>
2019-02-18 14:55:56 +0100

    ttkHarmonicFieldComputation: Update doxygen comment

--- core/vtk/ttkHarmonicFieldComputation/ttkHarmonicFieldComputation.h
+++ core/vtk/ttkHarmonicFieldComputation/ttkHarmonicFieldComputation.h
@@ -1,33 +1,18 @@
 /// \ingroup vtk
 /// \class ttkHarmonicFieldComputation
-/// \author Guillaume Favelier <guillaume.favelier@lip6.fr>
-/// \date February 2016
+/// \author Pierre Guillou <pierre.guillou@lip6.fr>
+/// \date February 2019
 ///
-/// \brief TTK VTK-filter for the topological simplification of scalar
-/// data.
+/// \brief TTK VTK-filter for harmonic field computations.
 ///
-/// Given an input scalar field and a list of critical points to remove, this
-/// filter minimally edits the scalar field such that the listed critical points
-/// disappear. This procedure is useful to speedup subsequent topological data
-/// analysis when outlier critical points can be easily identified. It is
-/// also useful for data simplification.
+/// The current filter takes a list of sources with attached scalar
+/// values and produces a scalar harmonic field fulfilling these
+/// constraints.
 ///
-/// The list of critical points to remove must be associated with a point data
-/// scalar field that represent the vertex global identifiers in the input
-/// geometry.
-///
-/// Note that this filter will also produce an output vertex offset scalar field
-/// that can be used for further topological data analysis tasks to disambiguate
-/// vertices on flat plateaus. For instance, this output vertex offset field
-/// can specified to the ttkFTMTree, vtkIntegralLines, or
-/// vtkScalarFieldCriticalPoints filters.
-///
-/// Also, this filter can be given a specific input vertex offset.
-///
-/// \param Input0 Input scalar field, either 2D or 3D, either regular grid or
-/// triangulation (vtkDataSet)
+/// \param Input0 Input geometry, either 2D or 3D, either regular grid
+/// or triangulation (vtkDataSet)
 /// \param Input1 List of critical point constraints (vtkPointSet)
-/// \param Output Output simplified scalar field (vtkDataSet)
+/// \param Output Output harmonic scalar field (vtkDataSet)
 ///
 /// This filter can be used as any other VTK filter (for instance, by using the
 /// sequence of calls SetInputData(), Update(), GetOutput()).
@@ -36,18 +21,17 @@
 /// VTK pipeline.
 ///
 /// \b Related \b publication \n
-/// "Generalized Topological Simplification of Scalar Fields on Surfaces" \n
-/// Julien Tierny, Valerio Pascucci \n
-/// Proc. of IEEE VIS 2012.\n
-/// IEEE Transactions on Visualization and Computer Graphics, 2012.
+/// "Dynamic harmonic fields for surface processing" \n
+/// Kai Xu, Hao Zhang, Daniel Cohen-Or, Yueshan Xiong \n
+/// Computers & Graphics 2009. \n
 ///
 /// \sa ttkScalarFieldCriticalPoints
 /// \sa ttkIntegralLines
 /// \sa ttkFTMTree
 /// \sa ttkIdentifiers
 /// \sa ttk::HarmonicFieldComputation
-#ifndef _TTK_HARMONICFIELDCOMPUTATION_H
-#define _TTK_HARMONICFIELDCOMPUTATION_H
+
+#pragma once
 
 // VTK includes -- to adapt
 #include <vtkCharArray.h>
@@ -154,5 +138,3 @@ private:
   vtkDataArray *offsets_;
   vtkDataArray *inputOffsets_;
 };
-
-#endif // _TTK_HARMONICFIELDCOMPUTATION_H
