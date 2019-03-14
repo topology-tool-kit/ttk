ecbde5a2388e2e10a6d67359a955ebcbb7467cf1
Pierre Guillou <pierre.guillou@lip6.fr>
2019-03-14 11:40:26 +0100

    [SurfaceQuadrangulation] Simplify quadrangles lookup algorithm

--- core/base/surfaceQuadrangulation/SurfaceQuadrangulation.cpp
+++ core/base/surfaceQuadrangulation/SurfaceQuadrangulation.cpp
@@ -28,16 +28,15 @@ int ttk::SurfaceQuadrangulation::execute() const {
 
   // separatrices sources -> list of destinations mapping
   vector<vector<SimplexId>> sepMappingSources(sepMappingSet.size());
-  // separatrices destinations -> list of sources mapping
-  vector<vector<SimplexId>> sepMappingDests(sepMappingSet.size());
 
   for(auto &p : sepMappingSet) {
     for(SimplexId i = 0; i < criticalPointsNumber_; i++) {
       if(p.first == criticalPointsCellIds_[i]) {
-        sepMappingSources[i].emplace_back(p.second);
-      }
-      if(p.second == criticalPointsCellIds_[i]) {
-        sepMappingDests[i].emplace_back(p.first);
+        for(SimplexId j = 0; j < criticalPointsNumber_; j++) {
+          if(p.second == criticalPointsCellIds_[j]) {
+            sepMappingSources[i].emplace_back(j);
+          }
+        }
       }
     }
   }
@@ -47,63 +46,54 @@ int ttk::SurfaceQuadrangulation::execute() const {
 #pragma omp parallel for num_threads(threadNumber_)
 #endif // TTK_ENABLE_OPENMP
   for(size_t i = 0; i < sepMappingSources.size(); i++) {
-    for(auto &dest : sepMappingSources[i]) {
-      // index of dest in the critical points array
-      SimplexId j;
-      for(j = 0; j < criticalPointsNumber_; j++) {
-        if(dest == criticalPointsCellIds_[j]) {
+    for(auto &j : sepMappingSources[i]) {
+      // find in sources the ones that have j as dest
+      size_t k;
+      for(k = 0; k < sepMappingSources.size(); k++) {
+        if(k == i) {
+          continue;
+        }
+        auto beg = sepMappingSources[k].begin();
+        auto end = sepMappingSources[k].end();
+        if(std::find(beg, end, j) != end) {
           break;
         }
       }
-      // all the sources that lead to dest
-      auto dest_sources = sepMappingDests[j];
-      for(auto &src : dest_sources) {
-        // filter out outer loop source
-        if(src == criticalPointsCellIds_[i]) {
-          continue;
-        }
-        // index of the current src
-        SimplexId k;
-        for(k = 0; k < criticalPointsNumber_; k++) {
-          if(src == criticalPointsCellIds_[k]) {
-            break;
-          }
-        }
-        // find a second common dest of i and k (fourth vertex)
-        vector<SimplexId> common_dests;
-        std::set_intersection(
-          sepMappingSources[i].begin(), sepMappingSources[i].end(),
-          sepMappingSources[k].begin(), sepMappingSources[k].end(),
-          std::back_inserter(common_dests));
-        if(common_dests.size() > 1) {
-          // gotcha!
-          SimplexId l;
-          for(auto &other_dest : common_dests) {
-            if(other_dest == dest) {
-              // skip second vertex
-              continue;
-            }
-            // find index of fourth vertex
-            for(l = 0; l < criticalPointsNumber_; l++) {
-              if(other_dest == criticalPointsCellIds_[l]) {
-                break;
-              }
-            }
-            // we don't need more than four vertices
-            break;
-          }
+      // skip this iteration if no k found
+      if(k == sepMappingSources.size()) {
+        continue;
+      }
+      // list of common dests to i and k
+      vector<SimplexId> common_dests;
+      std::set_intersection(
+        sepMappingSources[i].begin(), sepMappingSources[i].end(),
+        sepMappingSources[k].begin(), sepMappingSources[k].end(),
+        std::back_inserter(common_dests));
 
-          // fill output vector one thread at a time
+      // find a fourth vertex that is a common dest to i and k but not j
+      if(common_dests.size() > 1) {
+        // gotcha!
+        SimplexId l;
+        for(auto &other_dest : common_dests) {
+          if(other_dest == j) {
+            // skip second vertex
+            continue;
+          }
+          l = other_dest;
+          // we don't need more than four vertices
+          break;
+        }
+
+        // fill output vector one thread at a time
 #ifdef TTK_ENABLE_OPENMP
 #pragma omp critical
 #endif // TTK_ENABLE_OPENMP
-          {
-            outputCells_->emplace_back(4);
-            outputCells_->emplace_back(i);
-            outputCells_->emplace_back(j);
-            outputCells_->emplace_back(k);
-            outputCells_->emplace_back(l);
-          }
+        {
+          outputCells_->emplace_back(4);
+          outputCells_->emplace_back(i);
+          outputCells_->emplace_back(j);
+          outputCells_->emplace_back(k);
+          outputCells_->emplace_back(l);
         }
       }
     }
@@ -119,14 +109,6 @@ int ttk::SurfaceQuadrangulation::execute() const {
     cout << endl;
   }
   cout << endl;
-
-  for(SimplexId i = 0; i < criticalPointsNumber_; i++) {
-    cout << criticalPointsCellIds_[i] << " ";
-    for(auto &v : sepMappingDests[i]) {
-      cout << v << " ";
-    }
-    cout << endl;
-  }
 
   // output only segments linking two critical points
   for(auto &p : sepMappingSet) {
