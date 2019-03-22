b8b9adb7ef0763e70efb19dd2487b55950f1db45
Pierre Guillou <pierre.guillou@lip6.fr>
2019-03-22 12:39:59 +0100

    [QuadrangulationSubdivision] Regroup vector insertions
    Avoid using pointers after inserting into vector

--- core/base/quadrangulationSubdivision/QuadrangulationSubdivision.cpp
+++ core/base/quadrangulationSubdivision/QuadrangulationSubdivision.cpp
@@ -27,38 +27,43 @@ int ttk::QuadrangulationSubdivision::subdivise(
     // middles of edges
     auto ij = std::make_pair(q.i, q.j);
     auto midij = (*pi + *pj) * 0.5;
+    auto jk = std::make_pair(q.j, q.k);
+    auto midjk = (*pj + *pk) * 0.5;
+    auto kl = std::make_pair(q.k, q.l);
+    auto midkl = (*pk + *pl) * 0.5;
+    auto li = std::make_pair(q.l, q.i);
+    auto midli = (*pl + *pi) * 0.5;
+
+    // quad barycenter
+    auto bary = (*pi + *pj + *pk + *pl) * 0.25;
+
+    // add to outputPoints_ after computing new point coordinates to
+    // avoid invalidating pointers
     if(processedEdges.find(ij) == processedEdges.end()) {
       processedEdges.insert(
         std::make_pair(ij, std::make_pair(outputPoints_->size(), midij)));
       outputPoints_->emplace_back(midij);
     }
 
-    auto jk = std::make_pair(q.j, q.k);
-    auto midjk = (*pj + *pk) * 0.5;
     if(processedEdges.find(jk) == processedEdges.end()) {
       processedEdges.insert(
         std::make_pair(jk, std::make_pair(outputPoints_->size(), midjk)));
       outputPoints_->emplace_back(midjk);
     }
 
-    auto kl = std::make_pair(q.k, q.l);
-    auto midkl = (*pk + *pl) * 0.5;
     if(processedEdges.find(kl) == processedEdges.end()) {
       processedEdges.insert(
         std::make_pair(kl, std::make_pair(outputPoints_->size(), midkl)));
       outputPoints_->emplace_back(midkl);
     }
 
-    auto li = std::make_pair(q.l, q.i);
-    auto midli = (*pl + *pi) * 0.5;
     if(processedEdges.find(li) == processedEdges.end()) {
       processedEdges.insert(
         std::make_pair(li, std::make_pair(outputPoints_->size(), midli)));
       outputPoints_->emplace_back(midli);
     }
 
-    // quad barycenter
-    auto bary = (*pi + *pj + *pk + *pl) * 0.25;
+    // barycenter index in outputPoints_
     long long baryIdx = outputPoints_->size();
     outputPoints_->emplace_back(bary);
 
