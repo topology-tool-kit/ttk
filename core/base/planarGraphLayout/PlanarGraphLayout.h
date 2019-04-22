951109195605f7bd19545667cf1e242efde663f2
Pierre Guillou <guillou@cri.ensmp.fr>
2019-04-22 13:10:04 +0200

    [PlanarGraphLayout] Extract graphviz dependency into .cpp
    Fix leaking graphviz dependency into vtk layer.

--- core/base/planarGraphLayout/PlanarGraphLayout.h
+++ core/base/planarGraphLayout/PlanarGraphLayout.h
@@ -24,11 +24,6 @@
 // base code includes
 #include <Wrapper.h>
 
-#if TTK_ENABLE_GRAPHVIZ
-#include <graphviz/cgraph.h>
-#include <graphviz/gvc.h>
-#endif
-
 using namespace std;
 
 namespace ttk{
@@ -102,54 +97,7 @@ namespace ttk{
 
                 // Output
                 float* layout
-            ) const {
-                #if TTK_ENABLE_GRAPHVIZ
-                    Timer t;
-
-                    dMsg(cout, "[ttkPlanarGraphLayout] Computing layout ... ", timeMsg);
-
-                    // ---------------------------------------------------------
-                    // Init GraphViz
-                    // ---------------------------------------------------------
-                    Agraph_t* G = agmemread( dotString.data() );
-                    GVC_t* gvc = gvContext();
-                    gvLayout(gvc, G, "dot");
-
-                    // ---------------------------------------------------------
-                    // Get layout data from GraphViz
-                    // ---------------------------------------------------------
-                    for(auto i: nodeIndicies){
-                        Agnode_t* n = agnode(G, const_cast<char*>(to_string(i).data()), 0);
-                        if(n!=nullptr){
-                            auto& coord = ND_coord(n);
-                            size_t offset = i*2;
-                            layout[offset] = coord.x/72; // points to inches
-                            layout[offset+1] = coord.y/72; // points to inches
-                        }
-                    }
-
-                    // ---------------------------------------------------------
-                    // Free GraphViz memory
-                    // ---------------------------------------------------------
-                    gvFreeLayout(gvc, G);
-                    agclose(G);
-                    gvFreeContext(gvc);
-
-                    // ---------------------------------------------------------
-                    // Print status
-                    // ---------------------------------------------------------
-                    {
-                        stringstream msg;
-                        msg << "done (" << t.getElapsedTime() <<  " s)" << endl;
-                        dMsg(cout, msg.str(), timeMsg);
-                    }
-
-                    return 1;
-                #else
-                    dMsg(cout, "[ttkPlanarGraphLayout] ERROR: This filter requires GraphViz to compute a layout.\n", fatalMsg);
-                    return 0;
-                #endif
-            };
+            ) const;
     };
 }
 
