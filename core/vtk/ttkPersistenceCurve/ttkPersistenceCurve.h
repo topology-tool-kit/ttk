0c51af6ace8187ef07da045393db91495547c25b
Pierre Guillou <pierre.guillou@lip6.fr>
2019-05-22 12:35:08 +0200

    [PersistenceCurve] Outline switch body

--- core/vtk/ttkPersistenceCurve/ttkPersistenceCurve.h
+++ core/vtk/ttkPersistenceCurve/ttkPersistenceCurve.h
@@ -106,6 +106,9 @@ public:
   int getTriangulation(vtkDataSet *input);
   int getOffsets(vtkDataSet *input);
 
+  template <typename VTK_TT>
+  int dispatch();
+
 protected:
   ttkPersistenceCurve();
   ~ttkPersistenceCurve();
