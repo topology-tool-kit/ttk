52caefadb5d5e1911099055d07989a1f5ed8414d
Pierre Guillou <pierre.guillou@lip6.fr>
2019-07-01 13:20:33 +0200

    [SurfaceQuadrangulation] Merge small cells according to seps

--- core/base/surfaceQuadrangulation/SurfaceQuadrangulation.cpp
+++ core/base/surfaceQuadrangulation/SurfaceQuadrangulation.cpp
@@ -303,6 +303,72 @@ int ttk::SurfaceQuadrangulation::quadrangulate(size_t &ndegen) {
     }
   }
 
+  // fuse two (small) cells sharing their cellId and at least three separatrices
+
+  // detect cells sharing the same cell id
+  std::vector<std::vector<size_t>> sharedCells{};
+  for(size_t i = 0; i < cellId_.size(); ++i) {
+    // check if i not already included
+    for(const auto &s : sharedCells) {
+      if(std::find(s.begin(), s.end(), i) != s.end()) {
+        continue;
+      }
+    }
+    std::set<size_t> shared_i{i};
+    for(size_t j = i + 1; j < cellId_.size(); ++j) {
+      if(cellId_[j] == cellId_[i]) {
+        shared_i.emplace(j);
+      }
+    }
+    if(shared_i.size() > 1) {
+      sharedCells.emplace_back(shared_i.begin(), shared_i.end());
+    }
+  }
+
+  for(const auto &c : sharedCells) {
+    // try to find at least three common separatrices
+    std::vector<size_t> last(cellSeps_[c[0]]);
+    std::vector<size_t> curr{};
+    for(size_t i = 1; i < c.size(); ++i) {
+      std::set_intersection(last.begin(), last.end(), cellSeps_[c[i]].begin(),
+                            cellSeps_[c[i]].end(), std::back_inserter(curr));
+      std::swap(last, curr);
+      curr.clear();
+    }
+    if(last.size() > 2) {
+      // we should merge everything into c[0]
+      for(size_t i = 1; i < c.size(); ++i) {
+        for(size_t j = 0; j < segmentationNumber_; ++j) {
+          if(morseSeg_[j] == static_cast<SimplexId>(c[i])) {
+            morseSeg_[j] = c[0];
+          }
+        }
+      }
+      // search for separatrices in the whole area
+      std::map<size_t, int> hist{};
+      for(size_t i = 0; i < segmentationNumber_; ++i) {
+        if(morseSeg_[i] != static_cast<SimplexId>(c[0])) {
+          continue;
+        }
+        auto nneigh = triangulation_->getVertexNeighborNumber(i);
+        for(SimplexId j = 0; j < nneigh; ++j) {
+          SimplexId next;
+          triangulation_->getVertexNeighbor(i, j, next);
+          if(onSep[next] != -1) {
+            hist[onSep[next]]++;
+          }
+        }
+      }
+      auto sortedSeps = sortHistWeight(hist);
+      cellSeps_[c[0]] = sortedSeps;
+      // cleanup merged cells
+      for(size_t i = 1; i < c.size(); ++i) {
+        cellId_.erase(std::next(cellId_.begin(), c[i]));
+        cellSeps_.erase(std::next(cellSeps_.begin(), c[i]));
+      }
+    }
+  }
+
   // hold quad subdivision
   outputCells_.reserve(5 * cellSeps_.size());
   auto quads = reinterpret_cast<std::vector<Quad> *>(&outputCells_);
