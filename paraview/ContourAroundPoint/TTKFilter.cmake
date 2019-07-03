9933d588c0767432298bd8ec773ec3f92ef5a050
Charles Gueunet <charles.gueunet@gmail.com>
2019-07-03 09:20:44 +0200

    chore(Update): Migrate the ttkContourAroundPoint filter to new structure
    This commit can be used as an example on how to migrate a module from
    the old to the new architecture.
    A guideline on the subject will certainly come in the near future

---
+++ paraview/ContourAroundPoint/TTKFilter.cmake
@@ -0,0 +1,8 @@
+# Allows to disable each filter
+option(TTK_BUILD_CONTOUR_AROUND_POINT_FILTER "Build the CountourAroundPoint filter" ON)
+mark_as_advanced(TTK_BUILD_CONTOUR_AROUND_POINT_FILTER)
+
+if(${TTK_BUILD_CONTOUR_AROUND_POINT_FILTER})
+  ttk_register_pv_filter(pvContourAroundPoint ttkContourAroundPoint)
+endif()
+
