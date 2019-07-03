93d8f79236b61d2993684b1705cb9e12802745ef
Charles Gueunet <charles.gueunet@gmail.com>
2019-07-03 13:35:53 +0200

    update(travis): use ParaView just before 5.7

--- .travis-setup-dependencies.sh
+++ .travis-setup-dependencies.sh
@@ -6,22 +6,33 @@
 
 MAKE_NUM_JOBS=2
 
-if [ "$TTK_BUILD_PARAVIEW_PLUGINS" == "ON" ]; then
-	wget "http://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.4&type=source&os=all&downloadFile=ParaView-v5.4.0.tar.gz" -O ParaView-v5.4.0.tar.gz
-	tar -xf ParaView-v5.4.0.tar.gz
-	cd ../paraview/patch
-	./patch-paraview-5.4.0.sh ../../ext/ParaView-v5.4.0/
-	cd ../../ext/ParaView-v5.4.0/
+# assert pwd == <ttk-folder>/ext
 
-	mkdir build
-	cd build
+# TODO: may want to install VTK only
+# if TTK_BUILD_PARAVIEW_PLUGINS is OFF
+if [ "$TTK_BUILD_VTK_WRAPPERS" == "ON" ]; then
+  mkdir ParaView-nightly
+  cd ParaView-nightly || exit 2
+  git init
+  git remote add origin "https://gitlab.kitware.com/paraview/paraview.git"
+  git fetch --depth 1 origin "864d18d1b5500cb4088ecdc56d2ffd9922c81121"
+  git checkout FETCH_HEAD
+  git submodule update --init --recursive
+  cd ..
 
-	cmake -DCMAKE_INSTALL_PREFIX=../install \
-		-DVTK_RENDERING_BACKEND=OpenGL2 \
-		-DPARAVIEW_ENABLE_PYTHON=ON \
-		-DPARAVIEW_INSTALL_DEVELOPMENT_FILES=ON \
-		-DPARAVIEW_QT_VERSION=4 ..
+  cd ../paraview/patch || exit 2
+  ./patch-paraview-5.7.0.sh ../../ext/ParaView-nightly/
+  cd ../../ext/ParaView-nightly/ || exit 2
 
-	cmake --build . --target install -- -j $MAKE_NUM_JOBS
+  mkdir build
+  cd build || exit 2
+
+  cmake -DCMAKE_INSTALL_PREFIX=../install \
+    -DVTK_RENDERING_BACKEND=OpenGL2 \
+    -DPARAVIEW_ENABLE_PYTHON=ON \
+    -DPARAVIEW_INSTALL_DEVELOPMENT_FILES=ON \
+    -DPARAVIEW_QT_VERSION=4 ..
+
+  cmake --build . --target install -- -j $MAKE_NUM_JOBS
 fi
 
