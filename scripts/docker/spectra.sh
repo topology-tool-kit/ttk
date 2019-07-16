e69b9a0a794edf1fbe65846b11feb16687f68d07
Christoph Garth <garth@cs.uni-kl.de>
2019-07-16 21:45:57 +0200

    fix Spectra installation in Docker base image

--- scripts/docker/spectra.sh
+++ scripts/docker/spectra.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
+SPECTRA_VERSION=0.8.1
+
+set -e 
+
 echo "### build Spectra ###"
 
 build_pkgs \
-	curl
+	curl \
+	ca-certificates
 
 # Spectra is header only -> fetch and extract includes directly to final location
 fetch_url https://codeload.github.com/yixuan/spectra/tar.gz/v0.8.1 \
-    | tar zx -C usr --strip-components 1 --include "*/include/*"
+    | tar zvx -C usr --strip-components 1 "spectra-${SPECTRA_VERSION}/include"
 
