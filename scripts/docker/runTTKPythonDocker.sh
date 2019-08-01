c27f7f6779677effda4f03842739557a946b0f27
Julien J Tierny <julien.tierny@gmail.com>
2019-08-01 15:12:01 +0200

    Python docker updates

--- scripts/docker/runTTKPythonDocker.sh
+++ scripts/docker/runTTKPythonDocker.sh
@@ -2,10 +2,4 @@
 
 PARAVIEW_VERSION=5.6.1
 
-if [ -z "${PARAVIEW_PATH}" ]; then
-  echo "Usage:"
-  echo "  $0 [<Standard pvpython arguments (Python script, data, etc.)>]"
-  exit 0
-fi
-
 docker run -it --rm -p 11111:11111 -v "${HOME}:/home/${USER}/" --user ${UID} topologytoolkit/ttk:${PARAVIEW_VERSION}-master pvpython ${@:1}
