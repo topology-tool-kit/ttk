17e0d467feca2098eed54cc2a1fef02ca9e52445
Pierre Guillou <pierre.guillou@lip6.fr>
2019-09-30 17:28:16 +0200

    [PersistenceDiagramClustering] Mark overriden methods

--- core/vtk/ttkPersistenceDiagramClustering/ttkPersistenceDiagramClustering.h
+++ core/vtk/ttkPersistenceDiagramClustering/ttkPersistenceDiagramClustering.h
@@ -270,8 +270,8 @@ protected:
                                const double spacing,
                                const int diagramNumber);
 
-  int FillInputPortInformation(int port, vtkInformation *info);
-  int FillOutputPortInformation(int port, vtkInformation *info);
+  int FillInputPortInformation(int port, vtkInformation *info) override;
+  int FillOutputPortInformation(int port, vtkInformation *info) override;
 
   template <typename dataType>
   vtkSmartPointer<vtkUnstructuredGrid>
@@ -297,7 +297,7 @@ protected:
 
   int RequestData(vtkInformation *request,
                   vtkInformationVector **inputVector,
-                  vtkInformationVector *outputVector);
+                  vtkInformationVector *outputVector) override;
 
 private:
   // std::vector<std::vector<diagramTuple>>
@@ -348,9 +348,9 @@ private:
            vtkUnstructuredGrid *outputMatchings,
            int numInputs);
 
-  bool needsToAbort();
+  bool needsToAbort() override;
 
-  int updateProgress(const float &progress);
+  int updateProgress(const float &progress) override;
 
   template <typename T>
   int dispatch(int numInputs,
