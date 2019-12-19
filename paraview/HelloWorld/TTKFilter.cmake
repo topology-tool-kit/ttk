da45c86118c22237bc981226c05907848aae007f
Jonas Lukasczyk <jl@jluk.de>
2019-12-19 15:58:03 +0100

    Added ttkAlgorithm and ttkHelloWorld ParaView layer

--- paraview/HelloWorld/TTKFilter.cmake
+++ paraview/HelloWorld/TTKFilter.cmake
@@ -1,7 +1,1 @@
-# Allows to disable each filter
-option(TTK_BUILD_HELLOWORLD_FILTER "Build the HelloWorld filter" ${TTK_ENABLE_FILTER_DEFAULT})
-mark_as_advanced(TTK_BUILD_HELLOWORLD_FILTER)
-
-if(${TTK_BUILD_HELLOWORLD_FILTER})
-  ttk_register_pv_filter(ttkHelloWorld HelloWorld.xml)
-endif()
+ttk_register_pv_filter(ttkHelloWorld HelloWorld.xml)
