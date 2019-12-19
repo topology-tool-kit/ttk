8bdcb3988e213b49bb98788b96b2d4b1e5b17c69
Julien J Tierny <julien.tierny@gmail.com>
2019-12-19 05:57:19 +0100

    Adding helloWorld base class

---
+++ core/base/helloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <HelloWorld.h>
+
+using namespace std;
+using namespace ttk;
+using namespace helloWorld;
+
+HelloWorld::HelloWorld() {
+
+  inputData_ = NULL;
+  outputData_ = NULL;
+  triangulation_ = NULL;
+}
+
+HelloWorld::~HelloWorld() {
+}
