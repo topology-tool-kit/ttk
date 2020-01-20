6685b8145e3d6194da11406aa8c80a336427700c
Pierre Guillou <pierre.guillou@lip6.fr>
2020-01-20 14:12:22 +0100

    [python/example.py] New/Temporary Python import syntax

--- examples/python/example.py
+++ examples/python/example.py
@@ -27,12 +27,19 @@ from vtk import (
     vtkXMLUnstructuredGridWriter,
 )
 
-from topologytoolkit import (
-    ttkMorseSmaleComplex,
-    ttkPersistenceCurve,
-    ttkPersistenceDiagram,
-    ttkTopologicalSimplification,
-)
+# temporary fix (till topologytoolkit package re-exports all its modules)
+from topologytoolkit.ttkMorseSmaleComplex import *
+from topologytoolkit.ttkPersistenceCurve import *
+from topologytoolkit.ttkPersistenceDiagram import *
+from topologytoolkit.ttkTopologicalSimplification import *
+
+## old import syntax
+# from topologytoolkit import (
+#     ttkMorseSmaleComplex,
+#     ttkPersistenceCurve,
+#     ttkPersistenceDiagram,
+#     ttkTopologicalSimplification,
+# )
 
 if len(sys.argv) == 2:
     inputFilePath = sys.argv[1]
