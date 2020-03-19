2173a9977df0ef722aba63299f22f57a3b0bb654
Pierre Guillou <pierre.guillou@lip6.fr>
2020-03-19 19:45:42 +0100

    Clone StringArrayConverter into PointsToLine

---
+++ core/vtk/ttkPointsToLine/ttkPointsToLine.h
@@ -0,0 +1,45 @@
+/// \class ttkPointsToLine
+/// \ingroup vtk
+/// \author Jonas Lukasczyk <jl@jluk.de>
+/// \date 01.09.2018
+///
+/// \brief TTK VTK-filter that reads a Cinema Spec D Database.
+///
+/// This filter can be used as any other VTK filter (for instance, by using the
+/// sequence of calls SetInputData(), Update(), GetOutput()).
+///
+/// \param Output content of the data.csv file of the database in form of a
+/// vtkTable
+
+#pragma once
+
+// Module include
+#include <ttkPointsToLineModule.h>
+
+// VTK includes
+#include <ttkAlgorithm.h>
+#include <vtkInformation.h>
+
+class TTKPOINTSTOLINE_EXPORT ttkPointsToLine : public ttkAlgorithm {
+
+public:
+  static ttkPointsToLine *New();
+  vtkTypeMacro(ttkPointsToLine, ttkAlgorithm);
+
+  vtkSetMacro(InputStringArray, std::string);
+  vtkGetMacro(InputStringArray, std::string);
+
+protected:
+  ttkPointsToLine();
+  ~ttkPointsToLine() = default;
+
+  int FillInputPortInformation(int port, vtkInformation *info) override;
+  int FillOutputPortInformation(int port, vtkInformation *info) override;
+
+  int RequestData(vtkInformation *request,
+                  vtkInformationVector **inputVector,
+                  vtkInformationVector *outputVector) override;
+
+private:
+  std::string InputStringArray{};
+};
---
+++ paraview/PointsToLine/TTKFilter.cmake
@@ -0,0 +1,1 @@
+ttk_register_pv_filter(ttkPointsToLine PointsToLine.xml)
