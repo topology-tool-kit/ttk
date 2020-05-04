bf62a77d7cde8f5028f970bd90fd1cc3eb61082f
Pierre Guillou <pierre.guillou@lip6.fr>
2020-05-04 19:29:11 +0200

    [Auction] Use Geometry powInt function (through KDTree dependency)

--- core/base/auction/AuctionActor.h
+++ core/base/auction/AuctionActor.h
@@ -1,4 +1,3 @@
-
 #ifndef _AUCTIONACTOR_H
 #define _AUCTIONACTOR_H
 
@@ -76,22 +75,6 @@ namespace ttk {
       return (var >= 0) ? var : -var;
     }
 
-    inline dataType pow(const dataType var, int n) {
-      if(n < 0) {
-        return 1.0 / this->pow(var, -n);
-      } else if(n == 0) {
-        return 1;
-      } else if(n == 1) {
-        return var;
-      } else if(n == 2) {
-        return var * var;
-      } else if(n == 3) {
-        return var * var * var;
-      } else {
-        return this->pow(var, n - 1) * var;
-      }
-    }
-
     dataType
       cost(const AuctionActor &g, int &wasserstein, double &geometricalFactor);
 
@@ -151,9 +134,9 @@ namespace ttk {
   template <typename dataType>
   double AuctionActor<dataType>::getPairGeometricalLength(
     const int wasserstein) const {
-    return std::pow(geom_pair_length_[0], wasserstein)
-           + std::pow(geom_pair_length_[1], wasserstein)
-           + std::pow(geom_pair_length_[2], wasserstein);
+    return Geometry::powInt(geom_pair_length_[0], wasserstein)
+           + Geometry::powInt(geom_pair_length_[1], wasserstein)
+           + Geometry::powInt(geom_pair_length_[2], wasserstein);
   }
 
   template <typename dataType>
@@ -165,23 +148,26 @@ namespace ttk {
     } else if(is_diagonal_) {
       return geometricalFactor
                * (2
-                  * this->pow(abs<dataType>(g.y_ / 2 - g.x_ / 2), wasserstein))
+                  * Geometry::powInt(
+                    abs<dataType>(g.y_ / 2 - g.x_ / 2), wasserstein))
              + (1 - geometricalFactor) * getPairGeometricalLength(wasserstein);
     } else if(g.isDiagonal()) {
       return geometricalFactor
-               * (2 * this->pow(abs<dataType>(y_ / 2 - x_ / 2), wasserstein))
+               * (2
+                  * Geometry::powInt(
+                    abs<dataType>(y_ / 2 - x_ / 2), wasserstein))
              + (1 - geometricalFactor)
                  * g.getPairGeometricalLength(wasserstein);
     } else {
       return geometricalFactor
-               * (this->pow(abs<dataType>(x_ - g.x_), wasserstein)
-                  + this->pow(abs<dataType>(y_ - g.y_), wasserstein))
+               * (Geometry::powInt(abs<dataType>(x_ - g.x_), wasserstein)
+                  + Geometry::powInt(abs<dataType>(y_ - g.y_), wasserstein))
              + (1 - geometricalFactor)
-                 * (this->pow(
+                 * (Geometry::powInt(
                       abs<dataType>(coords_x_ - g.coords_x_), wasserstein)
-                    + this->pow(
+                    + Geometry::powInt(
                       abs<dataType>(coords_y_ - g.coords_y_), wasserstein)
-                    + this->pow(
+                    + Geometry::powInt(
                       abs<dataType>(coords_z_ - g.coords_z_), wasserstein));
     }
   }
@@ -416,7 +402,7 @@ namespace ttk {
       this->SetCriticalCoordinates(coords_x, coords_y, coords_z);
 
       if(std::abs(static_cast<double>(x) - static_cast<double>(y))
-         < std::pow(10, -12)) {
+         < Geometry::powIntTen<double>(-12)) {
         AuctionActor<dataType>::is_diagonal_ = true;
       } else {
         AuctionActor<dataType>::is_diagonal_ = false;
