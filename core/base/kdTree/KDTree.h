f419e28fd104a5ba5ccddedb6ffeb5e00ecc10aa
Pierre Guillou <pierre.guillou@lip6.fr>
2020-05-30 23:08:16 +0200

    [KDTree] Use std::unique_ptr to replace manual allocations

--- core/base/kdTree/KDTree.h
+++ core/base/kdTree/KDTree.h
@@ -23,59 +23,41 @@ namespace ttk {
   template <typename dataType>
   class KDTree : public Debug {
 
+  protected:
+    // Boolean indicating if the current node is a left node of its parent
+    bool is_left_;
+    // Indicates according to which coordinate the tree splits its elements
+    int coords_number_{0};
+    // Power used for the computation of distances. p=2 yields euclidean
+    // distance
+    int p_{2};
+    // Wether or not the KDTree should include weights that add up to distance
+    // for the computation of nearest neighbours
+    bool include_weights_{false};
+
   public:
-    KDTree *left_; // Lower half for the coordinate specified
-    KDTree *right_; // Higher half
-    KDTree *parent_;
-    int id_; // ID of the object saved here. The whole object is not kept in the
-             // KDTree Users should keep track of them in a table for instance
-    std::vector<dataType> coordinates_;
-    std::vector<dataType> coords_min_;
-    std::vector<dataType> coords_max_;
-    int level_;
+    std::unique_ptr<KDTree> left_{}; // Lower half for the coordinate specified
+    std::unique_ptr<KDTree> right_{}; // Higher half
+    KDTree *parent_{};
+    // ID of the object saved here. The whole object is not kept in the KDTree
+    // Users should keep track of them in a table for instance
+    int id_{};
+    std::vector<dataType> coordinates_{};
+    std::vector<dataType> coords_min_{};
+    std::vector<dataType> coords_max_{};
+    int level_{};
 
-    std::vector<dataType> weight_;
-    std::vector<dataType> min_subweights_;
+    std::vector<dataType> weight_{};
+    std::vector<dataType> min_subweights_{};
 
-    KDTree() {
-      left_ = nullptr;
-      right_ = nullptr;
-      parent_ = nullptr;
-
-      coords_number_ = 0;
-      p_ = 2;
-      include_weights_ = false;
+    KDTree() = default;
+    KDTree(bool include_weights, int p)
+      : p_{p}, include_weights_{include_weights} {
     }
 
-    KDTree(bool include_weights, int p) {
-      left_ = nullptr;
-      right_ = nullptr;
-      parent_ = nullptr;
-
-      coords_number_ = 0;
-      p_ = p;
-      include_weights_ = include_weights;
-    }
-
-    KDTree(KDTree *father, int coords_number, bool is_left) {
-      left_ = nullptr;
-      right_ = nullptr;
-      parent_ = father;
-
-      coords_number_ = coords_number;
-      p_ = father->p_;
-      include_weights_ = father->include_weights_;
-      is_left_ = is_left;
-    }
-
-    ~KDTree() {
-      delete left_;
-      delete right_;
-      // coordinates_.resize(0);
-      // coords_min_.resize(0);
-      // coords_max_.resize(0);
-      // weight_.resize(0);
-      // min_subweights_.resize(0);
+    KDTree(KDTree *father, int coords_number, bool is_left)
+      : is_left_{is_left}, coords_number_{coords_number},
+        include_weights_{father->include_weights_}, parent_{father} {
     }
 
     std::vector<KDTree<dataType> *> build(dataType *coordinates,
@@ -119,7 +101,7 @@ namespace ttk {
                               const int weight_index = 0);
 
     dataType cost(const std::vector<dataType> &coordinates);
-    dataType distanceToBox(KDTree<dataType> *subtree,
+    dataType distanceToBox(const KDTree<dataType> &subtree,
                            const std::vector<dataType> &coordinates);
     std::vector<dataType> getCoordinates();
     dataType getWeight(const int weight_index = 0);
@@ -132,19 +114,6 @@ namespace ttk {
     inline static type abs(const type var) {
       return (var > 0) ? var : -var;
     }
-
-  protected:
-    bool is_left_; // Boolean indicating if the current node is a left node of
-                   // its parent
-    int coords_number_; // Indicates according to which coordinate the tree
-                        // splits its elements
-
-    int p_; // Power used for the computation of distances. p=2 yields euclidean
-            // distance
-
-    bool include_weights_; // Wether or not the KDTree should include weights
-                           // that add up to distance for the computation of
-                           // nearest neighbours
   };
 
   template <typename dataType>
@@ -206,10 +175,10 @@ namespace ttk {
         idx_left.push_back(idx[i]);
       }
 
-      KDTree *left = new KDTree(this, (coords_number_ + 1) % dimension, true);
-      left->buildRecursive(data, idx_left, ptNumber, dimension, this,
-                           correspondance_map, weight_number);
-      left_ = left;
+      this->left_ = std::unique_ptr<KDTree>(
+        new KDTree(this, (coords_number_ + 1) % dimension, true));
+      this->left_->buildRecursive(data, idx_left, ptNumber, dimension, this,
+                                  correspondance_map, weight_number);
     }
 
     if(idx.size() > 1) {
@@ -218,10 +187,10 @@ namespace ttk {
       for(int i = 0; i < ptNumber - median_loc - 1; i++) {
         idx_right[i] = idx[i + median_loc + 1];
       }
-      KDTree *right = new KDTree(this, (coords_number_ + 1) % dimension, false);
-      right->buildRecursive(data, idx_right, ptNumber, dimension, this,
-                            correspondance_map, weight_number);
-      right_ = right;
+      this->right_ = std::unique_ptr<KDTree>(
+        new KDTree(this, (coords_number_ + 1) % dimension, false));
+      this->right_->buildRecursive(data, idx_right, ptNumber, dimension, this,
+                                   correspondance_map, weight_number);
     }
 
     return correspondance_map;
@@ -277,10 +246,10 @@ namespace ttk {
         idx_left[i] = idx_side[i];
       }
 
-      KDTree *left = new KDTree(this, (coords_number_ + 1) % dimension, true);
-      left->buildRecursive(data, idx_left, ptNumber, dimension, this,
-                           correspondance_map, weight_number);
-      left_ = left;
+      this->left_ = std::unique_ptr<KDTree>(
+        new KDTree(this, (coords_number_ + 1) % dimension, true));
+      this->left_->buildRecursive(data, idx_left, ptNumber, dimension, this,
+                                  correspondance_map, weight_number);
     }
 
     if(idx_side.size() > 1) {
@@ -289,10 +258,10 @@ namespace ttk {
       for(unsigned int i = 0; i < idx_side.size() - median_loc - 1; i++) {
         idx_right[i] = idx_side[i + median_loc + 1];
       }
-      KDTree *right = new KDTree(this, (coords_number_ + 1) % dimension, false);
-      right->buildRecursive(data, idx_right, ptNumber, dimension, this,
-                            correspondance_map, weight_number);
-      right_ = right;
+      this->right_ = std::unique_ptr<KDTree>(
+        new KDTree(this, (coords_number_ + 1) % dimension, false));
+      this->right_->buildRecursive(data, idx_right, ptNumber, dimension, this,
+                                   correspondance_map, weight_number);
     }
     return;
   }
@@ -392,7 +361,7 @@ namespace ttk {
     if(left_) {
       dataType max_cost = *std::max_element(costs.begin(), costs.end());
       dataType &min_subweight = left_->min_subweights_[weight_index];
-      dataType d_min = this->distanceToBox(left_, coordinates);
+      dataType d_min = this->distanceToBox(*left_, coordinates);
       if(costs.size() < k || d_min + min_subweight < max_cost) {
         // 2.2- It is possible that there exists a point in this subtree that is
         // less costly than max_cost
@@ -404,7 +373,7 @@ namespace ttk {
     if(right_) {
       dataType max_cost = *std::max_element(costs.begin(), costs.end());
       dataType &min_subweight = right_->min_subweights_[weight_index];
-      dataType d_min = this->distanceToBox(right_, coordinates);
+      dataType d_min = this->distanceToBox(*right_, coordinates);
       if(costs.size() < k || d_min + min_subweight < max_cost) {
         // 2.2- It is possible that there exists a point in this subtree that is
         // less costly than max_cost
@@ -426,16 +395,16 @@ namespace ttk {
 
   template <typename dataType>
   dataType
-    KDTree<dataType>::distanceToBox(KDTree<dataType> *subtree,
+    KDTree<dataType>::distanceToBox(const KDTree<dataType> &subtree,
                                     const std::vector<dataType> &coordinates) {
     dataType d_min = 0;
     for(size_t axis = 0; axis < coordinates.size(); axis++) {
-      if(subtree->coords_min_[axis] > coordinates[axis]) {
+      if(subtree.coords_min_[axis] > coordinates[axis]) {
         d_min
-          += Geometry::pow(subtree->coords_min_[axis] - coordinates[axis], p_);
-      } else if(subtree->coords_max_[axis] < coordinates[axis]) {
+          += Geometry::pow(subtree.coords_min_[axis] - coordinates[axis], p_);
+      } else if(subtree.coords_max_[axis] < coordinates[axis]) {
         d_min
-          += Geometry::pow(coordinates[axis] - subtree->coords_max_[axis], p_);
+          += Geometry::pow(coordinates[axis] - subtree.coords_max_[axis], p_);
       }
     }
     return d_min;
