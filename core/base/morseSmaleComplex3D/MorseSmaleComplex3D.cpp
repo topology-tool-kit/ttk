71f8ddc7e494f7e2a8f0b75552c915547d1679ec
Pierre Guillou <pierre.guillou@lip6.fr>
2020-06-09 09:36:52 +0200

    [MorseSmaleComplex3D] More parallelism in separatrices flattening

--- core/base/morseSmaleComplex3D/MorseSmaleComplex3D.cpp
+++ core/base/morseSmaleComplex3D/MorseSmaleComplex3D.cpp
@@ -311,25 +311,28 @@ void MorseSmaleComplex3D::flattenSeparatricesVectors(
   std::vector<std::vector<std::vector<ttk::dcg::Cell>>> &separatricesGeometry)
   const {
 
-    for(size_t i = 1; i < separatrices.size(); ++i) {
-      // shift separatrices geometry_
-      const auto offset = separatricesGeometry[0].size();
+  std::vector<size_t> partialSizes{0};
+  for(const auto &sep : separatrices) {
+    partialSizes.emplace_back(partialSizes.back() + sep.size());
+  }
+  separatrices[0].resize(partialSizes.back());
+  separatricesGeometry[0].resize(partialSizes.back());
+
+  for(size_t i = 1; i < separatrices.size(); ++i) {
+    const auto offset = partialSizes[i];
+
 #ifdef TTK_ENABLE_OPENMP
 #pragma omp parallel for num_threads(threadNumber_)
 #endif // TTK_ENABLE_OPENMP
-      for(size_t j = 0; j < separatrices[i].size(); ++j) {
-        for(size_t k = 0; k < separatrices[i][j].geometry_.size(); ++k) {
-          separatrices[i][j].geometry_[k] += offset;
-        }
+    for(size_t j = 0; j < separatrices[i].size(); ++j) {
+      // shift separatrices geometry_
+      for(size_t k = 0; k < separatrices[i][j].geometry_.size(); ++k) {
+        separatrices[i][j].geometry_[k] += offset;
       }
-
       // flatten separatrices1 and separatricesGeometry1
-      separatrices[0].insert(separatrices[0].end(),
-                              std::make_move_iterator(separatrices[i].begin()),
-                              std::make_move_iterator(separatrices[i].end()));
-      separatricesGeometry[0].insert(
-        separatricesGeometry[0].end(),
-        std::make_move_iterator(separatricesGeometry[i].begin()),
-        std::make_move_iterator(separatricesGeometry[i].end()));
+      separatrices[0][offset + j] = std::move(separatrices[i][j]);
+      separatricesGeometry[0][offset + j]
+        = std::move(separatricesGeometry[i][j]);
     }
+  }
 }
