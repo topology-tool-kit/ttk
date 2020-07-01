6ec14eb63eab138bbe4512d8f4d078185c60099d
Julien J Tierny <julien.tierny@gmail.com>
2020-07-01 09:22:24 +0200

    [ttkIdentifierRandomizer] vtk wrapper migration

--- core/vtk/ttkIdentifierRandomizer/ttkIdentifierRandomizer.h
+++ core/vtk/ttkIdentifierRandomizer/ttkIdentifierRandomizer.h
@@ -18,75 +18,36 @@
 #pragma once
 
 // VTK includes -- to adapt
-#include <vtkCellData.h>
-#include <vtkCharArray.h>
-#include <vtkDataArray.h>
-#include <vtkDataSet.h>
-#include <vtkDataSetAlgorithm.h>
-#include <vtkDoubleArray.h>
-#include <vtkFiltersCoreModule.h>
-#include <vtkFloatArray.h>
-#include <vtkInformation.h>
-#include <vtkIntArray.h>
-#include <vtkObjectFactory.h>
-#include <vtkPointData.h>
-#include <vtkSmartPointer.h>
 
 // VTK Module
 #include <ttkIdentifierRandomizerModule.h>
 
 // ttk code includes
-#include <ttkTriangulationAlgorithm.h>
+#include <ttkAlgorithm.h>
 
 // in this example, this wrapper takes a data-set on the input and produces a
 // data-set on the output - to adapt.
 // see the documentation of the vtkAlgorithm class to decide from which VTK
 // class your wrapper should inherit.
 class TTKIDENTIFIERRANDOMIZER_EXPORT ttkIdentifierRandomizer
-  : public vtkDataSetAlgorithm,
-    protected ttk::Wrapper {
+  : public ttkAlgorithm {
 
 public:
   static ttkIdentifierRandomizer *New();
-  vtkTypeMacro(ttkIdentifierRandomizer, vtkDataSetAlgorithm)
-
-    // default ttk setters
-    void SetDebugLevel(int debugLevel) {
-    setDebugLevel(debugLevel);
-    Modified();
-  }
-
-  void SetThreadNumber(int threadNumber) {
-    ThreadNumber = threadNumber;
-    SetThreads();
-  }
-  void SetUseAllCores(bool onOff) {
-    UseAllCores = onOff;
-    SetThreads();
-  }
-  // end of default ttk setters
-
-  vtkSetMacro(ScalarField, std::string);
-  vtkGetMacro(ScalarField, std::string);
+  vtkTypeMacro(ttkIdentifierRandomizer, ttkAlgorithm);
 
 protected:
-  ttkIdentifierRandomizer() {
+  ttkIdentifierRandomizer();
 
-    // init
-    outputScalarField_ = NULL;
+  ~ttkIdentifierRandomizer() override;
 
-    UseAllCores = true;
-  }
+  int FillInputPortInformation(int port, vtkInformation *info) override;
 
-  ~ttkIdentifierRandomizer() override {
-    if(outputScalarField_) {
-      outputScalarField_->Delete();
-    }
-  };
+  int FillOutputPortInformation(int port, vtkInformation *info) override;
 
-  TTK_SETUP();
+  int RequestData(vtkInformation *request,
+                  vtkInformationVector **inputVector,
+                  vtkInformationVector *outputVector) override;
 
 private:
-  std::string ScalarField;
-  vtkDataArray *outputScalarField_;
 };
