084ac5d8932ee6784b15c4d99efccac563d50492
Pierre Guillou <pierre.guillou@lip6.fr>
2020-11-27 15:35:52 +0100

    [set_windows_env_vars] Find ParaView, conda

--- scripts/set_windows_env_vars.ps1
+++ scripts/set_windows_env_vars.ps1
@@ -33,8 +33,30 @@ If ($args[0] -eq "install") {
     $main = ${function:remove_from_env_var}
 }
 
-& $main "PATH" "C:\Program Files\TTK\bin;C:\Program Files\TTK-ParaView\bin"
-& $main "PV_PLUGIN_PATH" "C:\Program Files\TTK\bin\plugins"
-& $main "QT_PLUGIN_PATH" "C:\ProgramData\Anaconda3\Library\plugins"
-& $main "PYTHONPATH ""C:\ProgramData\Anaconda3\Lib;C:\Program Files\TTK\bin\Lib\site-packages;C:\Program Files\TTK-ParaView\bin\Lib\site-packages"
-& $main "CMAKE_PREFIX_PATH" "C:\Program Files\TTK\lib\cmake;C:\Program Files\TTK-ParaView\lib\cmake"
+$ttk_dir = $MyInvocation.MyCommand.Definition | Split-Path | Split-Path
+$prefix = $ttk_dir | Split-Path
+# assumption: TTK and TTK-ParaView installed in the same prefix
+$pv_dir = "${prefix}\TTK-ParaView"
+
+function find_conda_dir() {
+    # assumption: Anaconda environment variables set
+    $python_exe = Get-Command python
+    If ($null -eq $python_exe) {
+        Write-Output "Error: Python executable not accessible in PATH"
+        exit
+    }
+    $conda_dir = $python_exe.Definition | Split-Path
+    If (-not ($conda_dir -Match "conda")) {
+        Write-Output "Error: Python not part of an Anaconda distribution"
+        exit
+    }
+    return $conda_dir
+}
+
+$conda_dir = find_conda_dir
+
+& $main "PATH" "${ttk_dir}\bin;${pv_dir}\bin"
+& $main "PV_PLUGIN_PATH" "${ttk_dir}\bin\plugins"
+& $main "QT_PLUGIN_PATH" "${conda_dir}\Library\plugins"
+& $main "PYTHONPATH" "${conda_dir}\Lib;${ttk_dir}\bin\Lib\site-packages;${pv_dir}\bin\Lib\site-packages"
+& $main "CMAKE_PREFIX_PATH" "${ttk_dir}\lib\cmake;${pv_dir}\lib\cmake"
