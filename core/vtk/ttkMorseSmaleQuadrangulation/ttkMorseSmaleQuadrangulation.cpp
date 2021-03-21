02d0ca4cb5a40b4c5d5cb665a003d2d8c4418bf1
Pierre Guillou <pierre.guillou@lip6.fr>
2021-03-21 16:30:05 +0100

    [MorseSmaleQuadrangulation] Cleaner data structures

--- core/vtk/ttkMorseSmaleQuadrangulation/ttkMorseSmaleQuadrangulation.cpp
+++ core/vtk/ttkMorseSmaleQuadrangulation/ttkMorseSmaleQuadrangulation.cpp
@@ -133,8 +133,8 @@ int ttkMorseSmaleQuadrangulation::RequestData(
 
   // vtkCellArray of quadrangle values containing outArray
   vtkNew<vtkCellArray> cells{};
-  for(size_t i = 0; i < outputCells_.size() / 5; i++) {
-    cells->InsertNextCell(4, &outputCells_[5 * i + 1]);
+  for(size_t i = 0; i < outputCells_.size(); i++) {
+    cells->InsertNextCell(4, outputCells_[i].data());
   }
 
   // update output: get quadrangle values
