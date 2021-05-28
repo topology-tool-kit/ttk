726a6ee1b8a52cd31367ba65555502b693c47e54
Pierre Guillou <pierre.guillou@lip6.fr>
2021-05-28 14:52:23 +0200

    [ProgressiveTopology] Use OpenMPLock.h

--- core/base/progressiveTopology/ProgressiveTopology.h
+++ core/base/progressiveTopology/ProgressiveTopology.h
@@ -24,6 +24,7 @@
 // base code includes
 #include <DynamicTree.h>
 #include <MultiresTriangulation.h>
+#include <OpenMPLock.h>
 #include <Triangulation.h>
 
 #include <tuple>
@@ -36,40 +37,6 @@ namespace ttk {
 
   using triplet = std::tuple<ttk::SimplexId, ttk::SimplexId, ttk::SimplexId>;
   using polarity = unsigned char;
-
-  /**
-   * @brief RAII wrapper around OpenMP lock
-   */
-  class Lock {
-#ifdef TTK_ENABLE_OPENMP
-  public:
-    Lock() {
-      omp_init_lock(&this->lock_);
-    }
-    ~Lock() {
-      omp_destroy_lock(&this->lock_);
-    }
-    inline void lock() {
-      omp_set_lock(&this->lock_);
-    }
-    inline void unlock() {
-      omp_unset_lock(&this->lock_);
-    }
-    Lock(const Lock &) = delete;
-    Lock(Lock &&) = delete;
-    Lock &operator=(const Lock &) = delete;
-    Lock &operator=(Lock &&) = delete;
-
-  private:
-    omp_lock_t lock_{};
-#else
-  public:
-    inline void lock() {
-    }
-    inline void unlock() {
-    }
-#endif // TTK_ENABLE_OPENMP
-  };
 
   /**
    * Compute the persistence diagram of a function on a triangulation.
