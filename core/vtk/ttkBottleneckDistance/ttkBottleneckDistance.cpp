02029decacc2d1043f44c819b8141ed7df79f4c1
Pierre Guillou <pierre.guillou@lip6.fr>
2021-11-06 11:43:32 +0100

    [BottleneckDistance] Extract shared function with TrackingFromPersistenceDiagrams

--- core/vtk/ttkBottleneckDistance/ttkBottleneckDistance.cpp
+++ core/vtk/ttkBottleneckDistance/ttkBottleneckDistance.cpp
@@ -1,8 +1,7 @@
 #include <ttkBottleneckDistance.h>
-#include <ttkPersistenceDiagramUtils.h>
+#include <ttkBottleneckDistanceUtils.h>
 #include <ttkUtils.h>
 
-#include <vtkCellData.h>
 #include <vtkDoubleArray.h>
 #include <vtkFloatArray.h>
 #include <vtkInformation.h>
@@ -39,41 +38,6 @@ int ttkBottleneckDistance::FillOutputPortInformation(int port,
     return 1;
   }
   return 0;
-}
-
-// Warn: this is duplicated in ttkTrackingFromPersistenceDiagrams
-int augmentDiagrams(const std::vector<ttk::MatchingType> &matchings,
-                    vtkUnstructuredGrid *const vtu0,
-                    vtkUnstructuredGrid *const vtu1) {
-
-  if(matchings.empty()) {
-    return 0;
-  }
-
-  vtkNew<vtkIntArray> matchingIds0{};
-  matchingIds0->SetName("MatchingIdentifier");
-  matchingIds0->SetNumberOfComponents(1);
-  matchingIds0->SetNumberOfTuples(vtu0->GetNumberOfCells());
-  vtu0->GetCellData()->AddArray(matchingIds0);
-
-  vtkNew<vtkIntArray> matchingIds1{};
-  matchingIds1->SetName("MatchingIdentifier");
-  matchingIds1->SetNumberOfComponents(1);
-  matchingIds1->SetNumberOfTuples(vtu1->GetNumberOfCells());
-  vtu1->GetCellData()->AddArray(matchingIds1);
-
-  // Unaffected by default
-  matchingIds0->Fill(-1);
-  matchingIds1->Fill(-1);
-
-  // Affect bottleneck matchings
-  for(size_t i = 0; i < matchings.size(); ++i) {
-    const auto &t = matchings[i];
-    matchingIds0->SetTuple1(std::get<0>(t), i);
-    matchingIds1->SetTuple1(std::get<1>(t), i);
-  }
-
-  return 1;
 }
 
 int translateDiagram(vtkUnstructuredGrid *output,
