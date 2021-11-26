d02321a170161680203621db52706512e06a4c11
Talha Bin Masood <talha.bin.masood@liu.se>
2021-11-26 09:48:05 +0100

    correction

--- core/vtk/ttkIcospheresFromPoints/ttkIcospheresFromPoints.h
+++ core/vtk/ttkIcospheresFromPoints/ttkIcospheresFromPoints.h
@@ -11,7 +11,7 @@
 ///
 /// \b Online \b examples: \n
 ///   - <a href="https://topology-tool-kit.github.io/examples/dragon/">Dragon
-/// example</a>
+/// example</a> \n
 ///   - <a href="https://topology-tool-kit.github.io/examples/morseMolecule/">
 /// Morse molecule example</a> \n
 ///
