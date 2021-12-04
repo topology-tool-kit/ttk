3580954dc52104d53712445278b7d185cf1bda60
Pierre Guillou <pierre.guillou@lip6.fr>
2021-12-04 13:50:38 +0100

    Let Doxygen handle splash.png

--- core/base/common/Debug.h
+++ core/base/common/Debug.h
@@ -1,7 +1,7 @@
 /// \namespace ttk The Topology ToolKit
 
 /// \mainpage TTK 1.0 Documentation
-/// \image html "../img/splash.png"
+/// \image html "splash.png"
 /// Useful links:
 ///   - TTK Home:
 /// <a href="https://topology-tool-kit.github.io/"
