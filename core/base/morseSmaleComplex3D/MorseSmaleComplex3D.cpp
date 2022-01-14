a7a857a687b40d801194ca4033e6a90a3488fd5b
Pierre Guillou <pierre.guillou@lip6.fr>
2022-01-14 23:14:05 +0100

    [MorseSmaleComplex] Gather methods into AbstractMorseSmaleComplex

--- core/base/morseSmaleComplex3D/MorseSmaleComplex3D.cpp
+++ core/base/morseSmaleComplex3D/MorseSmaleComplex3D.cpp
@@ -3,34 +3,3 @@
 ttk::MorseSmaleComplex3D::MorseSmaleComplex3D() : AbstractMorseSmaleComplex() {
   this->setDebugMsgPrefix("MorseSmaleComplex3D");
 }
-
-void ttk::MorseSmaleComplex3D::flattenSeparatricesVectors(
-  std::vector<std::vector<ttk::Separatrix>> &separatrices,
-  std::vector<std::vector<std::vector<ttk::dcg::Cell>>> &separatricesGeometry)
-  const {
-
-  std::vector<size_t> partialSizes{0};
-  for(const auto &sep : separatrices) {
-    partialSizes.emplace_back(partialSizes.back() + sep.size());
-  }
-  separatrices[0].resize(partialSizes.back());
-  separatricesGeometry[0].resize(partialSizes.back());
-
-  for(size_t i = 1; i < separatrices.size(); ++i) {
-    const auto offset = partialSizes[i];
-
-#ifdef TTK_ENABLE_OPENMP
-#pragma omp parallel for num_threads(threadNumber_)
-#endif // TTK_ENABLE_OPENMP
-    for(size_t j = 0; j < separatrices[i].size(); ++j) {
-      // shift separatrices geometry_
-      for(size_t k = 0; k < separatrices[i][j].geometry_.size(); ++k) {
-        separatrices[i][j].geometry_[k] += offset;
-      }
-      // flatten separatrices1 and separatricesGeometry1
-      separatrices[0][offset + j] = std::move(separatrices[i][j]);
-      separatricesGeometry[0][offset + j]
-        = std::move(separatricesGeometry[i][j]);
-    }
-  }
-}
