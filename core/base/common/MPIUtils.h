32288b687553422568c89c46c2f834e82bdd6d2a
Michael Will <will@delta-systeme.de>
2022-04-26 17:25:23 +0200

    added MPIUtils to CMakeList

--- core/base/common/MPIUtils.h
+++ core/base/common/MPIUtils.h
@@ -11,35 +11,35 @@
 
 namespace ttk {
 #ifdef TTK_ENABLE_MPI
-  MPI_Datatype getMPIType(const float ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const float ttkNotUsed(val)) {
     return MPI_FLOAT;
   };
-  MPI_Datatype getMPIType(const int ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const int ttkNotUsed(val)) {
     return MPI_INT;
   };
-  MPI_Datatype getMPIType(const unsigned int ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const unsigned int ttkNotUsed(val)) {
     return MPI_UNSIGNED;
   };
-  MPI_Datatype getMPIType(const double ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const double ttkNotUsed(val)) {
     return MPI_DOUBLE;
   };
-  MPI_Datatype getMPIType(const long double ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const long double ttkNotUsed(val)) {
     return MPI_LONG_DOUBLE;
   };
-  MPI_Datatype getMPIType(const long ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const long ttkNotUsed(val)) {
     return MPI_LONG;
   };
-  MPI_Datatype getMPIType(const unsigned long ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const unsigned long ttkNotUsed(val)) {
     return MPI_UNSIGNED_LONG;
   };
-  MPI_Datatype getMPIType(const long long ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const long long ttkNotUsed(val)) {
     return MPI_LONG_LONG;
   };
-  MPI_Datatype getMPIType(const unsigned long long ttkNotUsed(val)) {
+  inline MPI_Datatype getMPIType(const unsigned long long ttkNotUsed(val)) {
     return MPI_UNSIGNED_LONG_LONG;
   };
 
-  bool isRunningWithMPI() {
+  inline bool isRunningWithMPI() {
     int flag_i;
     MPI_Initialized(&flag_i);
     return flag_i;
