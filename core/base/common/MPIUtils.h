343b64b02f22765ab4b9b9d9117ce5e12d37ae61
Pierre Guillou <pierre.guillou@lip6.fr>
2022-05-13 14:31:19 +0200

    [MPIUtils] Add function to transfer vectors to a specific rank

--- core/base/common/MPIUtils.h
+++ core/base/common/MPIUtils.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <BaseClass.h>
+#include <vector>
 
 #if TTK_ENABLE_MPI
 #include <mpi.h>
@@ -46,6 +47,64 @@ namespace ttk {
     int flag_i;
     MPI_Initialized(&flag_i);
     return flag_i != 0;
+  }
+
+  /**
+   * @brief Gather vectors on a specific rank
+   *
+   * @param[out] dst Container storing vectors copied from other ranks
+   * @param[in] src Data to send to \ref destRank and to store in \ref dst
+   * @param[in] destRank Destination process identifier
+   * @return 0 in case of success
+   */
+  template <typename T>
+  int gatherVectors(std::vector<std::vector<T>> &dst,
+                    const std::vector<T> &src,
+                    const int destRank) {
+
+    if(!ttk::isRunningWithMPI()) {
+      return -1;
+    }
+
+    // src sizes (gathered on rank 0)
+    std::vector<unsigned long> vecSizes{};
+
+    if(ttk::MPIrank_ == destRank) {
+      vecSizes.resize(MPIsize_);
+      dst.resize(MPIsize_);
+    }
+
+    const unsigned long localSize = src.size();
+    // gather src sizes on destRank
+    MPI_Gather(&localSize, 1, MPI_UNSIGNED_LONG, vecSizes.data(), 1,
+               MPI_UNSIGNED_LONG, destRank, MPI_COMM_WORLD);
+
+    if(ttk::MPIrank_ == destRank) {
+      // allocate dst with vecSizes
+      for(int i = 0; i < ttk::MPIsize_; ++i) {
+        if(i == destRank) {
+          continue;
+        }
+        dst[i].resize(vecSizes[i]);
+      }
+
+      for(int i = 0; i < ttk::MPIsize_; ++i) {
+        if(i == destRank) {
+          continue;
+        }
+        // receive src content from other ranks
+        MPI_Recv(dst[i].data(), dst[i].size(), ttk::getMPIType(src[0]), i,
+                 MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      dst[destRank] = std::move(src);
+
+    } else {
+      // send src content to destRank
+      MPI_Send(src.data(), src.size(), ttk::getMPIType(src[0]), destRank, 0,
+               MPI_COMM_WORLD);
+    }
+
+    return 0;
   }
 
 } // namespace ttk
