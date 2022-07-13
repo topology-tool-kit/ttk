2b629e656d0e9b80d9998cd3fe58f49cac296aa7
Julien Tierny <julien.tierny@gmail.com>
2022-07-13 21:03:24 +0200

    mcp online examples

--- core/base/mandatoryCriticalPoints/MandatoryCriticalPoints.h
+++ core/base/mandatoryCriticalPoints/MandatoryCriticalPoints.h
@@ -22,6 +22,11 @@
 /// Computer Graphics Forum, 2014.
 ///
 /// \sa ttkMandatoryCriticalPoints.cpp %for a usage example.
+///
+/// \b Online \b examples: \n
+///   - <a
+///   href="https://topology-tool-kit.github.io/examples/uncertainStartingVortex/">
+///   Uncertain Starting Vortex example</a> \n
 
 #pragma once
 
