7216d67cf8879b8d04c325ad61c793bdceb184fe
lhofmann <lhofmann@google.com>
2022-07-21 18:46:49 +0200

    TTK dependency check is aware of the OPTIONAL_DEPENDS keyword.

--- CMake/VTKModule.cmake
+++ CMake/VTKModule.cmake
@@ -117,7 +117,7 @@ function(ttk_get_target ttk_module ttk_target)
 
   # dependencies check
   ttk_parse_module_file(${VTKWRAPPER_DIR}/${ttk_module}/vtk.module)
-  cmake_parse_arguments("VTK" "" "NAME" "DEPENDS;PRIVATE_DEPENDS" ${moduleFileContent})
+  cmake_parse_arguments("VTK" "" "NAME" "DEPENDS;PRIVATE_DEPENDS;OPTIONAL_DEPENDS" ${moduleFileContent})
   foreach(VTK_DEP_TARGET ${VTK_DEPENDS})
     # Check VTK targets are available (Assume VTK use the VTK namespace)
     string(REGEX MATCH "VTK::.*" TARGET_VTK ${VTK_DEP_TARGET})
