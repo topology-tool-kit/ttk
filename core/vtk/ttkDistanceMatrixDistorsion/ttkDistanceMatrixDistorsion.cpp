4b22238584137d34fe0e8bbdafe54b9f7da11c57
Alexandre Talon <alexandre.talon@lip6.fr>
2023-01-31 22:34:44 +0100

    The MatrixDistorsion module computing the SIMilarity between two distance matrices representing the same points. For instance, this can be used to check whether a dimension reduction algorithm preserves the distances between the points. Computes a fine-grained indicator: each vertex has its own distorsion value, in addition to the global distorsion value.

---
+++ core/vtk/ttkDistanceMatrixDistorsion/ttkDistanceMatrixDistorsion.cpp
@@ -0,0 +1,148 @@
+#include <ttkDistanceMatrixDistorsion.h>
+
+#include <vtkInformation.h>
+
+#include <vtkDataArray.h>
+#include <vtkDataSet.h>
+#include <vtkDoubleArray.h>
+#include <vtkObjectFactory.h>
+#include <vtkPointData.h>
+#include <vtkSmartPointer.h>
+#include <vtkTable.h>
+#include <vtkVariantArray.h>
+
+#include <regex>
+
+#include <ttkMacros.h>
+#include <ttkUtils.h>
+
+// A VTK macro that enables the instantiation of this class via ::New()
+// You do not have to modify this
+vtkStandardNewMacro(ttkDistanceMatrixDistorsion);
+
+ttkDistanceMatrixDistorsion::ttkDistanceMatrixDistorsion() {
+  this->SetNumberOfInputPorts(2);
+  this->SetNumberOfOutputPorts(1);
+}
+
+int ttkDistanceMatrixDistorsion::FillInputPortInformation(
+  int port, vtkInformation *info) {
+  if(port == 0) {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkTable");
+    return 1;
+  } else if(port == 1) {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkTable");
+    return 1;
+  }
+  return 0;
+}
+
+int ttkDistanceMatrixDistorsion::FillOutputPortInformation(
+  int port, vtkInformation *info) {
+  if(port == 0) {
+    info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkTable");
+    return 1;
+  }
+  return 0;
+}
+
+inline void fillWithInputColumns(vtkTable *input,
+                                 const std::string &regExpStr,
+                                 std::vector<std::string> &vectToFill) {
+  // Select all input columns whose name is matching the regexp.
+  vectToFill.clear();
+  const size_t n = input->GetNumberOfColumns();
+  for(size_t i = 0; i < n; ++i) {
+    const auto &name = input->GetColumnName(i);
+    if(std::regex_match(name, std::regex(regExpStr))) {
+      vectToFill.emplace_back(name);
+    }
+  }
+}
+
+int ttkDistanceMatrixDistorsion::RequestData(
+  vtkInformation *ttkNotUsed(request),
+  vtkInformationVector **inputVector,
+  vtkInformationVector *outputVector) {
+
+  // Get input objects from input vector
+  vtkTable *inputHigh = vtkTable::GetData(inputVector[0]);
+  vtkTable *inputLow = vtkTable::GetData(inputVector[1]);
+
+  vtkTable *output = vtkTable::GetData(outputVector);
+
+  if(!inputLow || !inputHigh || !output)
+    return 0;
+
+  // Selecting the columns names if a regexp was given.
+  if(SelectFieldsWithRegexpHigh)
+    fillWithInputColumns(inputHigh, RegexpStringHigh, ScalarFieldsHigh);
+  if(SelectFieldsWithRegexpLow)
+    fillWithInputColumns(inputLow, RegexpStringLow, ScalarFieldsLow);
+
+  const size_t nRowsHigh = inputHigh->GetNumberOfRows();
+  const size_t nColsHigh = ScalarFieldsHigh.size();
+  const size_t nRowsLow = inputLow->GetNumberOfRows();
+  const size_t nColsLow = ScalarFieldsLow.size();
+  if(nRowsHigh == 0 || nRowsHigh != nColsHigh) {
+    this->printErr("High input matrix is not a valid square matrix (rows: "
+                   + std::to_string(nRowsHigh)
+                   + ", columns: " + std::to_string(nColsHigh) + ")");
+    return 0;
+  }
+
+  if(nRowsLow == 0 || nRowsLow != nColsLow) {
+    this->printErr("Low input matrix is not a valid square matrix (rows: "
+                   + std::to_string(nRowsLow)
+                   + ", columns: " + std::to_string(nColsLow) + ")");
+    return 0;
+  }
+  if(nRowsHigh != nRowsLow) {
+    this->printErr(
+      "High and low input matrices must have same size (rows(high): "
+      + std::to_string(nRowsHigh) + ", rows(low): " + std::to_string(nRowsLow)
+      + ")");
+    return 0;
+  }
+
+  int n = nRowsHigh;
+  std::vector<std::vector<double>> vectMatHigh(n), vectMatLow(n);
+
+  // Getting the actual input columns.
+  std::vector<vtkAbstractArray *> arraysHigh{}, arraysLow{};
+  for(const auto &s : ScalarFieldsHigh)
+    arraysHigh.push_back(inputHigh->GetColumnByName(s.data()));
+  for(const auto &s : ScalarFieldsLow)
+    arraysLow.push_back(inputLow->GetColumnByName(s.data()));
+
+#ifdef TTK_ENABLE_OPENMP
+#pragma omp parallel for num_threads(this->threadNumber_)
+#endif // TTK_ENABLE_OPENMP
+  for(int i = 0; i < n; i++) {
+    vectMatHigh[i].resize(n);
+    vectMatLow[i].resize(n);
+    for(int j = 0; j < n; j++) {
+      vectMatHigh[i][j] = arraysHigh[j]->GetVariantValue(i).ToDouble();
+      vectMatLow[i][j] = arraysLow[j]->GetVariantValue(i).ToDouble();
+    }
+  }
+
+  std::vector<double> vectOutput;
+  double distorsionValue = 0;
+  this->printMsg("Starting computation of sim distorsion value...");
+  this->execute(vectMatHigh, vectMatLow, distorsionValue, vectOutput);
+
+  vtkNew<vtkDoubleArray> distorsionValArray{}, tmpCol{};
+  tmpCol->SetNumberOfTuples(n);
+  tmpCol->SetName("SimValue");
+  for(int i = 0; i < n; i++)
+    tmpCol->SetTuple1(i, vectOutput[i]);
+  output->AddColumn(tmpCol);
+
+  distorsionValArray->SetName("DistorsionValue");
+  distorsionValArray->SetNumberOfTuples(1);
+  distorsionValArray->SetTuple1(0, distorsionValue);
+  output->GetFieldData()->AddArray(distorsionValArray);
+
+  return 1;
+}
