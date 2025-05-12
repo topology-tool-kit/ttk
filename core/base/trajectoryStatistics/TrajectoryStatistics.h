4fbdec6a10ebc6ae61a261e627ed2a80b67416cc
Theophane LOLOUM <Theophane.LOLOUM@cea.fr>
2025-05-12 15:40:40 +0200

    Table en sortie avec frame début/fin, VX/VY moyen par trajectoire

--- core/base/trajectoryStatistics/TrajectoryStatistics.h
+++ core/base/trajectoryStatistics/TrajectoryStatistics.h
@@ -52,77 +52,17 @@ namespace ttk {
      *               method must be called after the triangulation has been
      *               preconditioned for the upcoming operations.
      */
-    template <class dataType,
-              class triangulationType = ttk::AbstractTriangulation>
-    int computeAverages(dataType *outputData,
-                        const dataType *inputData,
-                        const triangulationType *triangulation) const {
-      // start global timer
-      ttk::Timer globalTimer;
-
-      // print horizontal separator
-      this->printMsg(ttk::debug::Separator::L1); // L1 is the '=' separator
-
-      // print input parameters in table format
-      this->printMsg({
-        {"#Threads", std::to_string(this->threadNumber_)},
-        {"#Vertices", std::to_string(triangulation->getNumberOfVertices())},
-      });
-      this->printMsg(ttk::debug::Separator::L1);
-
-      // -----------------------------------------------------------------------
-      // Compute Vertex Averages
-      // -----------------------------------------------------------------------
-      {
-        // start a local timer for this subprocedure
-        ttk::Timer localTimer;
-
-        // print the progress of the current subprocedure (currently 0%)
-        this->printMsg("Computing Averages",
-                       0, // progress form 0-1
-                       0, // elapsed time so far
-                       this->threadNumber_, ttk::debug::LineMode::REPLACE);
-
-        // compute the average of each vertex in parallel
-        size_t const nVertices = triangulation->getNumberOfVertices();
-#ifdef TTK_ENABLE_OPENMP
-#pragma omp parallel for num_threads(this->threadNumber_)
-#endif
-        for(size_t i = 0; i < nVertices; i++) {
-          // initialize average
-          outputData[i] = inputData[i];
-
-          // add neighbor values to average
-          size_t const nNeighbors = triangulation->getVertexNeighborNumber(i);
-          ttk::SimplexId neighborId{-1};
-          for(size_t j = 0; j < nNeighbors; j++) {
-            triangulation->getVertexNeighbor(i, j, neighborId);
-            outputData[i] += inputData[neighborId];
-          }
-
-          // divide by neighbor number
-          outputData[i] /= (nNeighbors + 1);
-        }
-
-        // print the progress of the current subprocedure with elapsed time
-        this->printMsg("Computing Averages",
-                       1, // progress
-                       localTimer.getElapsedTime(), this->threadNumber_);
-      }
-
-      // ---------------------------------------------------------------------
-      // print global performance
-      // ---------------------------------------------------------------------
-      {
-        this->printMsg(ttk::debug::Separator::L2); // horizontal '-' separator
-        this->printMsg(
-          "Complete", 1, globalTimer.getElapsedTime() // global progress, time
-        );
-        this->printMsg(ttk::debug::Separator::L1); // horizontal '=' separator
-      }
-
-      return 1; // return success
-    }
+    int execute(std::vector<std::vector<int>> &trajTime,         //input
+                std::vector<std::vector<double>> &trajX,         //input 
+                std::vector<std::vector<double>> &trajY,         //input
+                std::vector<std::vector<double>> &trajZ,         //input 
+                std::vector<std::vector<int>>    &trajVertexId,   //input
+                std::vector<int> &startFrames,          //output
+                std::vector<int> &endFrames,            //output
+                std::vector<int> &durations,            //output
+                std::vector<double> &VX,
+                std::vector<double> &VY,
+                ttk::AbstractTriangulation*triangulation);
 
   }; // TrajectoryStatistics class
 
