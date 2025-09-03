9cc1c31e5b94753f553202977e4b37108e5b5834
Julien Tierny <julien.tierny@gmail.com>
2025-09-03 10:59:08 +0200

    [ddms] timing typo

--- core/base/discreteGradient/DiscreteGradient_Template.h
+++ core/base/discreteGradient/DiscreteGradient_Template.h
@@ -90,7 +90,7 @@ int DiscreteGradient::buildGradient(const triangulationType &triangulation,
         = ttk::endMPITimer(t_mpi, ttk::MPIrank_, ttk::MPIsize_);
       if(ttk::MPIrank_ == 0) {
         printMsg("Computation performed using " + std::to_string(ttk::MPIsize_)
-                 + " MPI processes lasted :" + std::to_string(elapsedTime));
+                 + " MPI processes lasted: " + std::to_string(elapsedTime));
       }
 #endif
     }
