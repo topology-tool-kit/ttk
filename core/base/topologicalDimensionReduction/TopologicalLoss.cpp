e6afc140a39ba171e18424ae27e623cb11d743f3
MClemot <matteo.clemot@ens-lyon.fr>
2025-12-15 16:39:27 +0100

    fix for W1 regularization

--- core/base/topologicalDimensionReduction/TopologicalLoss.cpp
+++ core/base/topologicalDimensionReduction/TopologicalLoss.cpp
@@ -32,11 +32,12 @@ torch::Tensor ttk::TopologicalLoss::computeLoss(const torch::Tensor &latent) {
 }
 
 void ttk::TopologicalLoss::precomputeInputPersistence() {
-  if(regul_ == REGUL::TOPOAE) {
+  if(regul_ == REGUL::TOPOAE || regul_ == REGUL::W_DIM1) {
     rpd::EdgeSets3 inputCritical;
     ripser::ripser(points_, inputCritical, rpd::inf, 0, false);
     inputCriticalPairIndices = {pairsToTorch(inputCritical[0])};
-  } else if(regul_ == REGUL::TOPOAE_DIM1) {
+  }
+  if(regul_ == REGUL::TOPOAE_DIM1) {
     rpd::EdgeSets3 inputCritical;
     ripser::ripser(points_, inputCritical, rpd::inf, 1, false);
     for(int i = 0; i <= 2; ++i)
@@ -45,12 +46,6 @@ void ttk::TopologicalLoss::precomputeInputPersistence() {
     ripser::ripser(points_, inputPD, rpd::inf, 1, false);
     auction = std::make_unique<
       PersistenceDiagramWarmRestartAuction<rpd::PersistencePair>>(inputPD[1]);
-#ifdef TTK_W1REG_WITH_TOPOAE0
-    // we add topoAE0 loss
-    rpd::EdgeSets3 inputCritical;
-    ripser::ripser(points_, inputCritical, rpd::inf, 0, false);
-    inputCriticalPairIndices[0] = pairsToTorch(inputCritical[0]);
-#endif
   } else if(regul_ == REGUL::CASCADE || regul_ == REGUL::ASYMMETRIC_CASCADE) {
     // first compute the PD with Ripser
     rpd::PairCellsWithOracle::callOracle(points_, inputPD);
