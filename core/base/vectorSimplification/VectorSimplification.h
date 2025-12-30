73bd0955edbdfbf06609e7c367d7e165ef68900e
Julien J Tierny <julien.tierny@gmail.com>
2025-12-30 09:23:20 +0100

    Revert "[dvft] clang-formatted edits"
    This reverts commit 1132fbc0ec9e77f7d668f23ec4a7ea2595628066.

--- core/base/vectorSimplification/VectorSimplification.h
+++ core/base/vectorSimplification/VectorSimplification.h
@@ -324,8 +324,8 @@ namespace ttk {
           options.pop();
         }
       }
-      this->printMsg("Simplified to " 
-                       + std::to_string(numCriticalPoints)
+      this->printMsg("Simplified to "
+                       + std::to_string(numCriticalPoints) 
                        + " critical point(s)",
                      1.0, tm.getElapsedTime(), this->threadNumber_);
 
@@ -649,32 +649,31 @@ void ttk::VectorSimplification::getAscSaddlePairs(
   const auto dim = this->dcvf_.getDimensionality();
 
   auto saddle2ToMaxima
-    = dim == 3 ? getSaddle2ToAscPair(
-                   criticalSaddles,
-                   [&triangulation](
-                     const SimplexId a, const SimplexId i, SimplexId &r) {
-                     return triangulation.getTriangleStar(a, i, r);
-                   },
-                   [&triangulation](const SimplexId a) {
-                     return triangulation.getTriangleStarNumber(a);
-                   },
-                   [&triangulation](const SimplexId a) {
-                     return triangulation.isTriangleOnBoundary(a);
-                   },
-                   triangulation, static_cast<dataType>(0.0))
-               : getSaddle2ToAscPair(
-                   criticalSaddles,
-                   [&triangulation](
-                     const SimplexId a, const SimplexId i, SimplexId &r) {
-                     return triangulation.getEdgeStar(a, i, r);
-                   },
-                   [&triangulation](const SimplexId a) {
-                     return triangulation.getEdgeStarNumber(a);
-                   },
-                   [&triangulation](const SimplexId a) {
-                     return triangulation.isEdgeOnBoundary(a);
-                   },
-                   triangulation, static_cast<dataType>(0.0));
+    = dim == 3
+        ? getSaddle2ToAscPair(
+          criticalSaddles,
+          [&triangulation](const SimplexId a, const SimplexId i, SimplexId &r) {
+            return triangulation.getTriangleStar(a, i, r);
+          },
+          [&triangulation](const SimplexId a) {
+            return triangulation.getTriangleStarNumber(a);
+          },
+          [&triangulation](const SimplexId a) {
+            return triangulation.isTriangleOnBoundary(a);
+          },
+          triangulation, static_cast<dataType>(0.0))
+        : getSaddle2ToAscPair(
+          criticalSaddles,
+          [&triangulation](const SimplexId a, const SimplexId i, SimplexId &r) {
+            return triangulation.getEdgeStar(a, i, r);
+          },
+          [&triangulation](const SimplexId a) {
+            return triangulation.getEdgeStarNumber(a);
+          },
+          [&triangulation](const SimplexId a) {
+            return triangulation.isEdgeOnBoundary(a);
+          },
+          triangulation, static_cast<dataType>(0.0));
 
   for(size_t i = 0; i < saddle2ToMaxima.size(); ++i) {
     auto &maxs = saddle2ToMaxima[i];
